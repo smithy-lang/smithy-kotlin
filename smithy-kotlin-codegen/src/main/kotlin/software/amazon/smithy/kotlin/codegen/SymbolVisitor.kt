e1317dfbf60eb663505d1b10db1de5cae6e2ff54
Ken Gilmer <kggilmer@amazon.com>
2020-09-21 15:22:41 -0700

    Add note regarding boxed primitives and nulls

--- smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/SymbolVisitor.kt
+++ smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/SymbolVisitor.kt
@@ -230,6 +230,7 @@ class SymbolVisitor(private val model: Model, private val rootNamespace: String 
 
     override fun mapShape(shape: MapShape): Symbol {
         val reference = toSymbol(shape.value)
+        // FIXME - this doesn't account for unboxed primitives
         return createSymbolBuilder(shape, "Map<String, ${reference.name}?>", boxed = true)
             .addReference(reference)
             .build()
