b69033e52fa79498719db7f394565f5c42f3b6e8
Ken Gilmer <kggilmer@amazon.com>
2020-10-29 17:31:07 -0700

    Update logging code to use amazon namespace.

--- client-runtime/logging/jvm/test/software/aws/clientrt/logging/internal/KLoggerNameResolverTest.kt
+++ client-runtime/logging/jvm/test/software/aws/clientrt/logging/internal/KLoggerNameResolverTest.kt
@@ -9,32 +9,32 @@ class KLoggerNameResolverTest {
 
     @Test
     fun testNames() {
-        assertEquals("mu.internal.BaseClass", KLoggerNameResolver.name(BaseClass::class.java))
-        assertEquals("mu.internal.ChildClass", KLoggerNameResolver.name(ChildClass::class.java))
-        assertEquals("mu.internal.BaseClass", KLoggerNameResolver.name(BaseClass::class.java))
-        assertEquals("mu.internal.ChildClass", KLoggerNameResolver.name(ChildClass::class.java))
-        assertEquals("mu.internal.Singleton", KLoggerNameResolver.name(Singleton::class.java))
-        assertEquals("mu.internal.MyInterface", KLoggerNameResolver.name(MyInterface::class.java))
+        assertEquals("software.aws.clientrt.logging.internal.BaseClass", KLoggerNameResolver.name(BaseClass::class.java))
+        assertEquals("software.aws.clientrt.logging.internal.ChildClass", KLoggerNameResolver.name(ChildClass::class.java))
+        assertEquals("software.aws.clientrt.logging.internal.BaseClass", KLoggerNameResolver.name(BaseClass::class.java))
+        assertEquals("software.aws.clientrt.logging.internal.ChildClass", KLoggerNameResolver.name(ChildClass::class.java))
+        assertEquals("software.aws.clientrt.logging.internal.Singleton", KLoggerNameResolver.name(Singleton::class.java))
+        assertEquals("software.aws.clientrt.logging.internal.MyInterface", KLoggerNameResolver.name(MyInterface::class.java))
         assertEquals("java.lang.Object", KLoggerNameResolver.name(Any().javaClass))
-        assertEquals("mu.internal.KLoggerNameResolverTest\$testNames$1", KLoggerNameResolver.name(object {}.javaClass))
+        assertEquals("software.aws.clientrt.logging.internal.KLoggerNameResolverTest\$testNames$1", KLoggerNameResolver.name(object {}.javaClass))
         assertEquals(
-            "mu.internal.BaseClass\$InnerClass\$Obj",
+            "software.aws.clientrt.logging.internal.BaseClass\$InnerClass\$Obj",
                 KLoggerNameResolver.name(BaseClass.InnerClass.Obj::class.java)
         )
         assertEquals(
-            "mu.internal.BaseClass\$InnerClass\$Obj",
+            "software.aws.clientrt.logging.internal.BaseClass\$InnerClass\$Obj",
                 KLoggerNameResolver.name(BaseClass.InnerClass.Obj.javaClass)
         )
         assertEquals(
-            "mu.internal.BaseClass\$InnerClass",
+            "software.aws.clientrt.logging.internal.BaseClass\$InnerClass",
                 KLoggerNameResolver.name(BaseClass.InnerClass::class.java)
         )
         assertEquals(
-            "mu.internal.BaseClass\$InnerClass",
+            "software.aws.clientrt.logging.internal.BaseClass\$InnerClass",
                 KLoggerNameResolver.name(BaseClass.InnerClass::class.java)
         )
-        assertEquals("mu.internal.Foo\$Bar", KLoggerNameResolver.name(Foo.Bar::class.java))
-        assertEquals("mu.internal.Foo\$Bar2", KLoggerNameResolver.name(Foo.Bar3.javaClass))
+        assertEquals("software.aws.clientrt.logging.internal.Foo\$Bar", KLoggerNameResolver.name(Foo.Bar::class.java))
+        assertEquals("software.aws.clientrt.logging.internal.Foo\$Bar2", KLoggerNameResolver.name(Foo.Bar3.javaClass))
     }
 }
 
