f6f6f7f0bbbbb4607b8f2d39570bc4b7a3be2440
Aaron Todd <aajtodd@users.noreply.github.com>
2021-03-18 13:09:49 -0400

    chore: bump version to 0.1.0-M0 (#255)

--- smithy-kotlin-codegen/build.gradle.kts
+++ smithy-kotlin-codegen/build.gradle.kts
@@ -35,9 +35,22 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVersion")
 }
 
+val generateSdkRuntimeVersion by tasks.registering {
+    // generate the version of the runtime to use as a resource.
+    // this keeps us from having to manually change version numbers in multiple places
+    val resourcesDir = "$buildDir/resources/main/software/amazon/smithy/kotlin/codegen"
+    val versionFile = file("$resourcesDir/sdk-version.txt")
+    outputs.file(versionFile)
+    sourceSets.main.get().output.dir(resourcesDir)
+    doLast {
+        versionFile.writeText("$version")
+    }
+}
+
 // unlike the client-runtime, smithy-kotlin codegen package is not expected to run on Android...we can target 1.8
 tasks.compileKotlin {
     kotlinOptions.jvmTarget = "1.8"
+    dependsOn(generateSdkRuntimeVersion)
 }
 
 tasks.compileTestKotlin {
