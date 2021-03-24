6217b1fb7941990dd640cb8cd7ef3f9b06906692
Aaron J Todd <todaaron@amazon.com>
2021-03-24 10:05:46 -0400

    fix nullability

--- client-runtime/client-rt-core/common/src/software/aws/clientrt/content/ByteArrayContent.kt
+++ client-runtime/client-rt-core/common/src/software/aws/clientrt/content/ByteArrayContent.kt
@@ -8,6 +8,6 @@ package software.aws.clientrt.content
  * Container for wrapping a ByteArray as a [ByteStream]
  */
 class ByteArrayContent(private val bytes: ByteArray) : ByteStream.Buffer() {
-    override val contentLength: Long? = bytes.size.toLong()
+    override val contentLength: Long = bytes.size.toLong()
     override fun bytes(): ByteArray = bytes
 }
