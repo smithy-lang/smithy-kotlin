dc63ee30e4d07706ae99a3b2830372f345aaa1c9
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2021-04-28 09:44:36 -0700

    Adding conversion extension methods between JVM's Instant and our Instant (#293)
    * Adding conversion extension methods between JVM's Instant and our Instant
    
    * Removing "Aws" from converter name. Adding missing header to test file.

---
+++ client-runtime/client-rt-core/jvm/src/software/aws/clientrt/time/Converters.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+package software.aws.clientrt.time
+
+/**
+ * Converts this [software.aws.clientrt.time.Instant] to a [java.time.Instant].
+ */
+fun Instant.toJvmInstant(): java.time.Instant = value
+
+/**
+ * Converts this [java.time.Instant] to a [software.aws.clientrt.time.Instant].
+ */
+fun java.time.Instant.toSdkInstant(): Instant = Instant(this)
