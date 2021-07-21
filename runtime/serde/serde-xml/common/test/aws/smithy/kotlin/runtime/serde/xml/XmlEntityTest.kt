2d8cada3fa38ed0c5268a6fd4e2de10b2f59fec9
Ken Gilmer <kggilmer@amazon.com>
2021-07-20 17:47:47 -0700

    lint

--- runtime/serde/serde-xml/common/test/aws/smithy/kotlin/runtime/serde/xml/XmlEntityTest.kt
+++ runtime/serde/serde-xml/common/test/aws/smithy/kotlin/runtime/serde/xml/XmlEntityTest.kt
@@ -1,10 +1,13 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
 package aws.smithy.kotlin.runtime.serde.xml
 
 import aws.smithy.kotlin.runtime.testing.runSuspendTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFails
-import kotlin.test.assertFailsWith
 
 class XmlEntityTest {
 
