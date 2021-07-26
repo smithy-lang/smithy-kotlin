d6e62dc573691ba429fa942b29e916503f263d7d
Aaron J Todd <todaaron@amazon.com>
2021-07-26 08:48:32 -0400

    feat(rt): add content-md5 middleware

---
+++ runtime/protocol/http/common/test/aws/smithy/kotlin/runtime/http/middleware/Md5ChecksumTest.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package aws.smithy.kotlin.runtime.http.middleware
+
+import aws.smithy.kotlin.runtime.http.Headers
+import aws.smithy.kotlin.runtime.http.HttpBody
+import aws.smithy.kotlin.runtime.http.HttpStatusCode
+import aws.smithy.kotlin.runtime.http.content.ByteArrayContent
+import aws.smithy.kotlin.runtime.http.engine.HttpClientEngineBase
+import aws.smithy.kotlin.runtime.http.operation.HttpOperationContext
+import aws.smithy.kotlin.runtime.http.operation.newTestOperation
+import aws.smithy.kotlin.runtime.http.operation.roundTrip
+import aws.smithy.kotlin.runtime.http.request.HttpRequest
+import aws.smithy.kotlin.runtime.http.request.HttpRequestBuilder
+import aws.smithy.kotlin.runtime.http.response.HttpCall
+import aws.smithy.kotlin.runtime.http.response.HttpResponse
+import aws.smithy.kotlin.runtime.http.sdkHttpClient
+import aws.smithy.kotlin.runtime.testing.runSuspendTest
+import aws.smithy.kotlin.runtime.time.Instant
+import aws.smithy.kotlin.runtime.util.get
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Md5ChecksumTest {
+    private val mockEngine = object : HttpClientEngineBase("test") {
+        override suspend fun roundTrip(request: HttpRequest): HttpCall {
+            val resp = HttpResponse(HttpStatusCode.OK, Headers.Empty, HttpBody.Empty)
+            return HttpCall(request, resp, Instant.now(), Instant.now())
+        }
+    }
+    private val client = sdkHttpClient(mockEngine)
+
+    @Test
+    fun itSetsContentMd5Header() = runSuspendTest {
+        val req = HttpRequestBuilder().apply {
+            body = ByteArrayContent("<Foo>bar</Foo>".encodeToByteArray())
+        }
+        val op = newTestOperation<Unit, Unit>(req, Unit)
+
+        op.install(Md5Checksum)
+
+        val expected = "RG22oBSZFmabBbkzVGRi4w=="
+        op.roundTrip(client, Unit)
+        val call = op.context.attributes[HttpOperationContext.HttpCallList].first()
+        assertEquals(expected, call.request.headers["Content-MD5"])
+    }
+}
