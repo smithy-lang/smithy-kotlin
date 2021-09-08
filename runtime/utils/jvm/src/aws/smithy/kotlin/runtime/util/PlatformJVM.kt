b453b9a6e3c069751ddd7057959823858377502d
Ken Gilmer <kggilmer@amazon.com>
2021-09-08 16:52:10 -0700

    Add logic to Platform type for AWS configuration use cases

--- runtime/utils/jvm/src/aws/smithy/kotlin/runtime/util/PlatformJVM.kt
+++ runtime/utils/jvm/src/aws/smithy/kotlin/runtime/util/PlatformJVM.kt
@@ -5,6 +5,8 @@
 
 package aws.smithy.kotlin.runtime.util
 
+import java.io.File
+import java.io.IOException
 import java.util.*
 
 public actual object Platform {
@@ -20,6 +22,32 @@ public actual object Platform {
     actual val isNative: Boolean = false
 
     actual fun osInfo(): OperatingSystem = getOsInfo()
+
+    /**
+     * Read the contents of a file as a [String] or return null on any error.
+     *
+     * @param path fully qualified path encoded specifically to the target platform's filesystem.
+     * @return contents of file or null if error (file does not exist, etc.)
+     */
+    actual fun readFile(path: String): String? = try {
+        File(path).readText()
+    } catch (e: IOException) {
+        null
+    }
+
+    /**
+     * Get a system property or null
+     *
+     * @param key name of environment variable
+     * @return value of system property or null if undefined or platform does not support properties
+     */
+    actual fun getProperty(key: String): String? = System.getProperty(key)
+
+    /**
+     * return the platform-specific file path separator char.  Eg on Linux a path may be '/root` and the path
+     * segment char is '/'.
+     */
+    actual val filePathSegment: String by lazy { File.separator }
 }
 
 private fun isAndroid(): Boolean = try {
