dd95278884b5406f418d642b61aab4181c2d5c99
Aaron J Todd <todaaron@amazon.com>
2021-09-16 16:17:13 -0400

    feat(rt): add conversion from HttpRequest -> builder

--- runtime/protocol/http/common/src/aws/smithy/kotlin/runtime/http/request/HttpRequest.kt
+++ runtime/protocol/http/common/src/aws/smithy/kotlin/runtime/http/request/HttpRequest.kt
@@ -4,10 +4,7 @@
  */
 package aws.smithy.kotlin.runtime.http.request
 
-import aws.smithy.kotlin.runtime.http.Headers
-import aws.smithy.kotlin.runtime.http.HttpBody
-import aws.smithy.kotlin.runtime.http.HttpMethod
-import aws.smithy.kotlin.runtime.http.Url
+import aws.smithy.kotlin.runtime.http.*
 
 /**
  * Immutable representation of an HTTP request
@@ -20,5 +17,27 @@ data class HttpRequest(
 ) {
     companion object {
         operator fun invoke(block: HttpRequestBuilder.() -> Unit): HttpRequest = HttpRequestBuilder().apply(block).build()
+    }
+}
+
+/**
+ * Convert an HttpRequest back to an [HttpRequestBuilder]
+ */
+fun HttpRequest.toBuilder(): HttpRequestBuilder {
+    val req = this
+    return HttpRequestBuilder().apply {
+        method = req.method
+        headers.appendAll(req.headers)
+        url {
+            scheme = req.url.scheme
+            host = req.url.host
+            port = req.url.port
+            path = req.url.path
+            parameters.appendAll(req.url.parameters)
+            fragment = req.url.fragment
+            userInfo = req.url.userInfo
+            forceQuery = req.url.forceQuery
+        }
+        body = req.body
     }
 }
