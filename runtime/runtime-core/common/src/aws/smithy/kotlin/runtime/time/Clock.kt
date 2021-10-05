76f754d0950c5581a675d7804254acb976e1899a
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2021-10-05 12:00:52 -0700

    feat: implement basic retry support in runtime (#487)

---
+++ runtime/runtime-core/common/src/aws/smithy/kotlin/runtime/time/Clock.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package aws.smithy.kotlin.runtime.time
+
+/**
+ * A source of time
+ */
+public interface Clock {
+    /**
+     * Get the current time from the clock source
+     */
+    public fun now(): Instant
+
+    /**
+     * A clock based on system time
+     */
+    public object System : Clock {
+        override fun now(): Instant = Instant.now()
+    }
+
+    public companion object {}
+}
