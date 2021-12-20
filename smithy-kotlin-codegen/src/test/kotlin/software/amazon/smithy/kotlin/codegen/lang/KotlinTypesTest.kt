83b7444bf8ee2caca0c166bc9974fcf7e50300f0
Ken Gilmer <kggilmer@amazon.com>
2021-12-20 12:11:43 -0800

    Additional unit tests

--- smithy-kotlin-codegen/src/test/kotlin/software/amazon/smithy/kotlin/codegen/lang/KotlinTypesTest.kt
+++ smithy-kotlin-codegen/src/test/kotlin/software/amazon/smithy/kotlin/codegen/lang/KotlinTypesTest.kt
@@ -4,7 +4,9 @@
  */
 package software.amazon.smithy.kotlin.codegen.lang
 
+import software.amazon.smithy.kotlin.codegen.model.buildSymbol
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -22,5 +24,23 @@ class KotlinTypesTest {
         listOf("", "aws-packg", "some random thing", "gar@bage?", "  white space").forEach {
             assertFalse(it.isValidPackageName())
         }
+    }
+
+    @Test
+    fun `it identifies single segment stdlib types`() {
+        val testSymbol = buildSymbol {
+            name = "String"
+            namespace = "kotlin"
+        }
+        assertEquals( true, testSymbol.isBuiltIn)
+    }
+
+    @Test
+    fun `it identifies multi-segment stdlib types`() {
+        val testSymbol = buildSymbol {
+            name = "Duration"
+            namespace = "kotlin.time"
+        }
+        assertEquals( true, testSymbol.isBuiltIn)
     }
 }
