596e089400c3c2a881dee145303d3a7bf861e098
Ken Gilmer <kggilmer@amazon.com>
2022-01-25 17:09:20 -0800

    Remove draft code from earlier version. Add notes to future refactoring

--- smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/rendering/GradleGenerator.kt
+++ smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/rendering/GradleGenerator.kt
@@ -12,8 +12,12 @@ import software.amazon.smithy.utils.CodeWriter
 // Determines the jvmTarget version emitted to the build file
 private val JVM_TARGET_VERSION: String = System.getProperty("smithy.kotlin.codegen.jvmTargetVersion", "1.8")
 
-// --------------- Option 1: use a template-style codegen to generate gradle files
-
+/**
+ * Generate Gradle build files
+ * @param settings [KotlinSettings] describe how the build files should be generated
+ * @param manifest Where the generated files are added
+ * @param dependencies list of [KotlinDependency]s for the project
+ */
 fun writeGradleBuild(
     settings: KotlinSettings,
     manifest: FileManifest,
@@ -194,7 +198,8 @@ private val repositoryRenderer: InlineCodeWriter = {
 }
 
 // Create a new [CodeWriter] for Gradle kts files
-// FIXME ~ new codewriter should use settings from parent
+// FIXME ~ new codewriter should use settings from parent. Support from Smithy is needed
+//  for this however.  See https://github.com/awslabs/smithy/issues/1066
 private fun createCodeWriter(): CodeWriter =
     CodeWriter().apply {
         trimBlankLines()
@@ -203,131 +208,3 @@ private fun createCodeWriter(): CodeWriter =
         expressionStart = '#'
         putFormatter('W', InlineCodeWriterFormatter(::createCodeWriter))
     }
-
-// --------------- Option 2: use default style codegen to generate gradle files
-
-/**
- * Create the gradle build file for the generated code
- */
-fun writeGradleBuildOldStyle(
-    settings: KotlinSettings,
-    manifest: FileManifest,
-    dependencies: List<KotlinDependency>
-) {
-    val writer = CodeWriter().apply {
-        trimBlankLines()
-        trimTrailingSpaces()
-        setIndentText("    ")
-        expressionStart = '#'
-    }
-
-    val isKmp = settings.build.generateMultiplatformProject
-
-    writer.withBlock("plugins {", "}\n") {
-        val projectModuleType = when (isKmp) {
-            true -> "multiplatform"
-            false -> "jvm"
-        }
-        if (settings.build.generateFullProject) {
-            write("kotlin(\"$projectModuleType\") version #S", KOTLIN_COMPILER_VERSION)
-        } else {
-            write("kotlin(\"$projectModuleType\")")
-        }
-    }
-
-    if (settings.build.generateFullProject) {
-        writer.withBlock("repositories {", "}\n") {
-            write("mavenLocal()")
-            write("mavenCentral()")
-        }
-    }
-
-    if (isKmp) { // In KMP projects the following generated code lives within a 'kotlin' block.
-        writer.withBlock("kotlin {", "}") {
-            withBlock("jvm {", "}") {
-                withBlock("compilations.all {", "}") {
-                    write("""kotlinOptions.jvmTarget = "$JVM_TARGET_VERSION"""")
-                }
-                withBlock("testRuns[\"test\"].executionTask.configure {", "}") {
-                    write("useJUnit()")
-                }
-            }
-            withBlock("sourceSets {", "}") {
-                withBlock("val commonMain by getting {", "}") {
-                    renderDependencies(writer, dependencies, isKmp) { !it.config.isTestScope }
-                }
-                if (dependencies.any { it.config.isTestScope }) {
-                    withBlock("val commonTest by getting {", "}") {
-                        renderDependencies(writer, dependencies, isKmp) { it.config.isTestScope }
-                    }
-                }
-                write("val jvmMain by getting")
-            }
-            renderAnnotations(writer, settings.build.optInAnnotations ?: emptyList())
-        }
-    } else {
-        renderDependencies(writer, dependencies, isKmp)
-        renderAnnotations(writer, settings.build.optInAnnotations ?: emptyList())
-    }
-
-    if (!isKmp) {
-        writer
-            .write("")
-            .withBlock("tasks.test {", "}") {
-                write("useJUnitPlatform()")
-                withBlock("testLogging {", "}") {
-                    write("""events("passed", "skipped", "failed")""")
-                    write("showStandardStreams = true")
-                }
-            }
-    }
-
-    val contents = writer.toString()
-    manifest.writeFile("build.gradle.kts", contents)
-    if (settings.build.generateFullProject) {
-        manifest.writeFile("settings.gradle.kts", "")
-    }
-}
-
-fun renderAnnotations(writer: CodeWriter, annotations: List<String>) {
-    if (annotations.isNotEmpty()) {
-        writer.withBlock("val optinAnnotations = listOf(", ")") {
-            call {
-                val formatted = annotations.joinToString(
-                    separator = ",\n",
-                    transform = {
-                        "\"$it\""
-                    }
-                )
-
-                writer.write(formatted)
-            }
-        }
-
-        writer.withBlock("kotlin.sourceSets.all {", "}") {
-            write("optinAnnotations.forEach { languageSettings.optIn(it) } ")
-        }
-    }
-}
-
-fun renderDependencies(
-    writer: CodeWriter,
-    dependencies: List<KotlinDependency>,
-    isKmp: Boolean,
-    filter: (KotlinDependency) -> Boolean = { true }
-) {
-    writer.withBlock("dependencies {", "}") {
-        if (!isKmp) {
-            write("implementation(kotlin(\"stdlib\"))")
-        }
-
-        // TODO - can we make kotlin dependencies not specify a version e.g. kotlin("kotlin-test")
-        // TODO - Kotlin MPP setup (pass through KotlinSettings) - maybe separate gradle writers
-        val orderedDependencies = dependencies.sortedWith(compareBy({ it.config }, { it.artifact }))
-        orderedDependencies
-            .filter(filter)
-            .forEach { dependency ->
-                write("${dependency.config}(\"#L:#L:#L\")", dependency.group, dependency.artifact, dependency.version)
-            }
-    }
-}
