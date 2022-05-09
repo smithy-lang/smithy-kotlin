183cda05380e790aca1da5af2ad4244c90818dd7
Ian Smith Botsford <issmith@amazon.com>
2022-05-09 21:20:43 +0000

    chore: add new AWS signer benchmarks

---
+++ tests/benchmarks/aws-signing-benchmarks/README.md
@@ -0,0 +1,24 @@
+# Serde Benchmarks
+
+This project contains benchmarks for the [AWS signer implementations](../../runtime/auth).
+
+## Testing
+
+```sh
+./gradlew :tests:benchmarks:aws-signing-benchmarks:benchmark
+```
+
+Baseline `0.9.0-beta` on EC2 **[m5.4xlarge](https://aws.amazon.com/ec2/instance-types/m5/)** in **OpenJK 1.8.0_312**:
+
+```
+jvm summary:
+Benchmark                            (signerName)  Mode  Cnt   Score   Error  Units
+AwsSignerBenchmark.signingBenchmark       default  avgt    5  33.592 ± 0.103  us/op
+AwsSignerBenchmark.signingBenchmark           crt  avgt    5  38.919 ± 0.494  us/op
+```
+
+## Test data
+
+The current test data consist of [a synthetic request with a 1KB payload][request-code].
+
+[request-code]: jvm/src/aws/smithy/kotlin/benchmarks/auth/signing/AwsSignerBenchmark.kt#L25-L40
---
+++ tests/benchmarks/aws-signing-benchmarks/jvm/src/aws/smithy/kotlin/benchmarks/auth/signing/AwsSignerBenchmark.kt
@@ -0,0 +1,60 @@
+package aws.smithy.kotlin.benchmarks.auth.signing
+
+import aws.smithy.kotlin.runtime.auth.awssigning.AwsSignedBodyHeader
+import aws.smithy.kotlin.runtime.auth.awssigning.AwsSigningConfig
+import aws.smithy.kotlin.runtime.auth.awssigning.crt.CrtAwsSigner
+import aws.smithy.kotlin.runtime.auth.awssigning.DefaultAwsSigner
+import aws.smithy.kotlin.runtime.auth.awssigning.tests.testCredentialsProvider
+import aws.smithy.kotlin.runtime.http.HttpBody
+import aws.smithy.kotlin.runtime.http.HttpMethod
+import aws.smithy.kotlin.runtime.http.Protocol
+import aws.smithy.kotlin.runtime.http.request.HttpRequest
+import aws.smithy.kotlin.runtime.http.request.headers
+import aws.smithy.kotlin.runtime.http.request.url
+import kotlinx.benchmark.*
+import kotlinx.coroutines.runBlocking
+
+private const val DEFAULT_SIGNER = "default"
+private const val CRT_SIGNER = "crt"
+
+private val signers = mapOf(
+    DEFAULT_SIGNER to DefaultAwsSigner,
+    CRT_SIGNER to CrtAwsSigner,
+)
+
+private val payload = (';'..'z').joinToString("").repeat(16).encodeToByteArray() // 1KB
+
+private val requestToSign = HttpRequest {
+    method = HttpMethod.GET
+    url {
+        scheme = Protocol.HTTPS
+        host = "foo.com"
+        path = "bar/baz/../qux/"
+        port = 8080
+    }
+    headers {
+        append("Content-Type", "application/x-www-form-urlencoded")
+        append("Cache-Control", "no-cache")
+    }
+    body = HttpBody.fromBytes(payload)
+}
+
+private val config = AwsSigningConfig {
+    region = "the-moon"
+    service = "fooservice"
+    signedBodyHeader = AwsSignedBodyHeader.X_AMZ_CONTENT_SHA256
+    credentialsProvider = testCredentialsProvider
+}
+
+@BenchmarkMode(Mode.AverageTime)
+@State(Scope.Benchmark)
+open class AwsSignerBenchmark {
+    @Param(DEFAULT_SIGNER, CRT_SIGNER)
+    var signerName: String = ""
+
+    @Benchmark
+    fun signingBenchmark() = runBlocking {
+        val signer = signers.getValue(signerName)
+        signer.sign(requestToSign, config)
+    }
+}
