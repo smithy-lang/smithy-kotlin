e8e4d0c1b7d721c2b7bcb2105c47d3a0d9a900ae
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2022-05-12 09:07:42 -0700

    feat: add support for getting all env vars and system props (#638)

--- runtime/utils/common/src/aws/smithy/kotlin/runtime/util/PropertyProvider.kt
+++ runtime/utils/common/src/aws/smithy/kotlin/runtime/util/PropertyProvider.kt
@@ -8,7 +8,13 @@ package aws.smithy.kotlin.runtime.util
 /**
  * Provide a mapping from (JVM) property key to value
  */
-public fun interface PropertyProvider {
+public interface PropertyProvider {
+    /**
+     * Get a map of all JVM system properties.
+     * @return A map of string keys to string values.
+     */
+    public fun getAllProperties(): Map<String, String>
+
     /**
      * Get a system property (on supported platforms)
      *
