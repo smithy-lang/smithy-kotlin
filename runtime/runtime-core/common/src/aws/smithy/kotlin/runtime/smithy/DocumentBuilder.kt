7ab7c81c02087a4ff1f0df619ecb0fc1140fa370
Luc Talatinian <102624213+lucix-aws@users.noreply.github.com>
2022-05-26 14:02:07 -0400

    feat: update design of smithy Document type (#648)

---
+++ runtime/runtime-core/common/src/aws/smithy/kotlin/runtime/smithy/DocumentBuilder.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+package aws.smithy.kotlin.runtime.smithy
+
+import kotlin.jvm.JvmName
+
+class DocumentBuilder internal constructor() {
+    val content: MutableMap<String, Document> = linkedMapOf()
+
+    infix fun String.to(value: Number?) {
+        require(content[this] == null) { "Key $this is already registered in builder" }
+        content[this] = if (value != null) Document(value) else Document.Null
+    }
+
+    infix fun String.to(value: String?) {
+        require(content[this] == null) { "Key $this is already registered in builder" }
+        content[this] = if (value != null) Document(value) else Document.Null
+    }
+
+    infix fun String.to(value: Boolean?) {
+        require(content[this] == null) { "Key $this is already registered in builder" }
+        content[this] = if (value != null) Document(value) else Document.Null
+    }
+
+    infix fun String.to(value: Document?) {
+        require(content[this] == null) { "Key $this is already registered in builder" }
+        content[this] = value ?: Document.Null
+    }
+
+    class ListBuilder internal constructor() {
+        val content: MutableList<Document> = mutableListOf()
+
+        fun add(value: Number?): Boolean =
+            content.add(if (value != null) Document(value) else Document.Null)
+        fun add(value: String?): Boolean =
+            content.add(if (value != null) Document(value) else Document.Null)
+        fun add(value: Boolean?): Boolean =
+            content.add(if (value != null) Document(value) else Document.Null)
+        fun add(value: Document?): Boolean =
+            content.add(value ?: Document.Null)
+
+        @JvmName("addAllNumbers") fun addAll(value: List<Number?>) = value.forEach(::add)
+        @JvmName("addAllStrings") fun addAll(value: List<String?>) = value.forEach(::add)
+        @JvmName("addAllBooleans") fun addAll(value: List<Boolean?>) = value.forEach(::add)
+        @JvmName("addAllDocuments") fun addAll(value: List<Document?>) = value.forEach(::add)
+    }
+
+    /**
+     * Builds a [Document] list with the given builder.
+     */
+    fun buildList(init: ListBuilder.() -> Unit): Document =
+        ListBuilder().let {
+            it.init()
+            Document(it.content)
+        }
+}
+
+/**
+ * Builds a [Document] with the given builder.
+ */
+fun buildDocument(init: DocumentBuilder.() -> Unit): Document =
+    DocumentBuilder().let {
+        it.init()
+        Document(it.content)
+    }
--- runtime/runtime-core/common/test/aws/smithy/kotlin/runtime/DocumentBuilderTest.kt
+++ runtime/runtime-core/common/test/aws/smithy/kotlin/runtime/DocumentBuilderTest.kt
@@ -5,25 +5,29 @@
 
 package aws.smithy.kotlin.runtime
 
-import aws.smithy.kotlin.runtime.smithy.document
-import aws.smithy.kotlin.runtime.smithy.documentArray
+import aws.smithy.kotlin.runtime.smithy.Document
+import aws.smithy.kotlin.runtime.smithy.buildDocument
 import kotlin.test.*
 
 class DocumentBuilderTest {
     @Test
     fun buildsAnObject() {
-        val doc = document {
+        val doc = buildDocument {
             "foo" to 1
-            "baz" to documentArray {
-                +n(202L)
-                +n(12)
-                +true
-                +"blah"
+            "baz" to buildList {
+                add(202L)
+                add(12)
+                add(true)
+                add("blah")
+                add(Document.Null)
+                addAll(listOf(9, 10, 12))
             }
+            "qux" to Document.Null
         }
 
-        val expected = """{"foo":1,"baz":[202,12,true,"blah"]}"""
+        val expected = """{"foo":1,"baz":[202,12,true,"blah",null,9,10,12],"qux":null}"""
 
         assertEquals(expected, "$doc")
+        assertEquals(1, doc.asMap()["foo"]?.asInt())
     }
 }
