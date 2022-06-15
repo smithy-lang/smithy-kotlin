021cca98e60d097db2393cb7ec737dec95bc7129
Luc Talatinian <lucix@amazon.com>
2022-06-15 14:21:36 -0400

    remove test obsoleted by document constructors

--- runtime/serde/serde-json/common/test/aws/smithy/kotlin/runtime/serde/json/JsonSerializerTest.kt
+++ runtime/serde/serde-json/common/test/aws/smithy/kotlin/runtime/serde/json/JsonSerializerTest.kt
@@ -9,7 +9,6 @@ import aws.smithy.kotlin.runtime.smithy.Document
 import aws.smithy.kotlin.runtime.smithy.buildDocument
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
 
 private val testAnonObjDescriptor = SdkObjectDescriptor.build { }
 
@@ -241,21 +240,6 @@ class JsonSerializerTest {
             Document(10.5),
             "10.5"
         )
-
-    @Test
-    fun cannotSerializeDocumentInfinity() {
-        val s = JsonSerializer()
-        val documentField = SdkFieldDescriptor(SerialKind.Document, JsonSerialName("SerializedDocument"))
-        val struct = SdkObjectDescriptor.build {
-            field(documentField)
-        }
-
-        assertFailsWith<SerializationException>("cannot serialize ") {
-            s.serializeStruct(struct) {
-                field(documentField, Document(Double.POSITIVE_INFINITY))
-            }
-        }
-    }
 
     @Test
     fun canSerializeDocumentStringField() =
