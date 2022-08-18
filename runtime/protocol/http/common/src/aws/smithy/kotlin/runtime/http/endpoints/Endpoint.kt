60561634e23bf2fd69bfcf8b5e9d740ce8b47634
Luc Talatinian <lucix@amazon.com>
2022-08-18 15:14:59 -0400

    make endpoint attribute construction internal

--- runtime/protocol/http/common/src/aws/smithy/kotlin/runtime/http/endpoints/Endpoint.kt
+++ runtime/protocol/http/common/src/aws/smithy/kotlin/runtime/http/endpoints/Endpoint.kt
@@ -6,20 +6,12 @@
 package aws.smithy.kotlin.runtime.http.endpoints
 
 import aws.smithy.kotlin.runtime.http.Url
+import aws.smithy.kotlin.runtime.util.InternalApi
 
 /**
  * Represents the endpoint a service client should make API operation calls to.
  *
  * The SDK will automatically resolve these endpoints per API client using an internal resolver.
- *
- * @property uri The base URL endpoint clients will use to make API calls to e.g. "api.myservice.com".
- * NOTE: Only `scheme`, `port`, `host` `path`, and `parameters` are valid. Other URL elements are ignored.
-
- * @property isHostnameImmutable Flag indicating that the hostname can be modified by the SDK client.
- *
- * @property attributes A grab-bag property map of endpoint attributes. The values here are considered unstable.
- *
- * @property headers A map of additional headers to be set when making calls against this endpoint.
  *
  * If the hostname is mutable the SDK clients may modify any part of the hostname based
  * on the requirements of the API (e.g. adding or removing content in the hostname).
@@ -31,11 +23,52 @@ import aws.smithy.kotlin.runtime.http.Url
  * is expected to be mutable and the client cannot modify the endpoint correctly, the operation
  * will likely fail.
  */
-public data class Endpoint(
-    public val uri: Url,
-    public val isHostnameImmutable: Boolean = false,
-    public val attributes: Map<String, Any> = mapOf(),
-    public val headers: Map<String, List<String>> = mapOf(),
-) {
+public class Endpoint {
+    /**
+     * The base URL endpoint clients will use to make API calls to e.g. "api.myservice.com".
+     * NOTE: Only `scheme`, `port`, `host` `path`, and `parameters` are valid. Other URL elements are ignored.
+     */
+    public val uri: Url
+
+    /**
+     * Flag indicating whether the hostname can be modified by the SDK client.
+     */
+    public val isHostnameImmutable: Boolean
+
+    /**
+     * A map of additional HTTP headers to be set when making calls against this endpoint.
+     */
+    public val headers: Map<String, List<String>>
+
+    /**
+     * A grab-bag property map of endpoint attributes. The values here are only set when the endpoint returned from
+     * evaluating a ruleset.
+     */
+    @InternalApi
+    public val attributes: Map<String, Any>
+
+    public constructor(
+        uri: Url,
+        isHostnameImmutable: Boolean = false,
+        headers: Map<String, List<String>> = mapOf(),
+    ) {
+        this.uri = uri
+        this.isHostnameImmutable = isHostnameImmutable
+        this.headers = headers
+        this.attributes = mapOf()
+    }
+
     public constructor(uri: String) : this(Url.parse(uri))
+    @InternalApi
+    public constructor(
+        uri: Url,
+        isHostnameImmutable: Boolean = false,
+        headers: Map<String, List<String>> = mapOf(),
+        attributes: Map<String, Any> = mapOf(),
+    ) {
+        this.uri = uri
+        this.isHostnameImmutable = isHostnameImmutable
+        this.headers = headers
+        this.attributes = attributes
+    }
 }
