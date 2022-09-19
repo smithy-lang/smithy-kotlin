b9b3a207ca058befe02dd579a136324338be956f
Luc Talatinian <102624213+lucix-aws@users.noreply.github.com>
2022-09-19 10:19:50 -0700

    feat: endpoints2: codegen for endpoint params (#699)

---
+++ smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/rendering/endpoints/EndpointParametersGenerator.kt
@@ -0,0 +1,196 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package software.amazon.smithy.kotlin.codegen.rendering.endpoints
+
+import software.amazon.smithy.codegen.core.Symbol
+import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
+import software.amazon.smithy.kotlin.codegen.core.withBlock
+import software.amazon.smithy.kotlin.codegen.lang.KotlinTypes
+import software.amazon.smithy.kotlin.codegen.model.boxed
+import software.amazon.smithy.kotlin.codegen.utils.doubleQuote
+import software.amazon.smithy.kotlin.codegen.utils.getOrNull
+import software.amazon.smithy.rulesengine.language.EndpointRuleset
+import software.amazon.smithy.rulesengine.language.eval.Value
+import software.amazon.smithy.rulesengine.language.lang.Identifier
+import software.amazon.smithy.rulesengine.language.lang.parameters.Parameter
+import software.amazon.smithy.rulesengine.language.lang.parameters.ParameterType
+
+private const val DEFAULT_DEPRECATED_MESSAGE =
+    "This field is deprecated and no longer recommended for use."
+
+private const val CLASS_NAME = "EndpointParameters"
+
+/**
+ * Renders the struct of parameters to be passed to the endpoint provider for resolution.
+ */
+class EndpointParametersGenerator(
+    private val writer: KotlinWriter,
+    rules: EndpointRuleset,
+) {
+    private val params: List<KotlinEndpointParameter> = rules.parameters.toList()
+        .sortedBy { it.name.toKotlin() }
+        .map {
+            KotlinEndpointParameter(
+                it.name.toKotlin(),
+                it.type.toSymbol(),
+                it.isRequired,
+                it.defaultValue.getOrNull()?.toKotlinLiteral() ?: "null",
+                it.documentation.getOrNull(),
+                it.deprecated.getOrNull(),
+            )
+        }
+
+    fun render() {
+        renderDocumentation()
+        writer.withBlock("public class #L private constructor(builder: Builder) {", "}", CLASS_NAME) {
+            renderFields()
+            renderCompanionObject()
+            write("")
+            renderEquals()
+            write("")
+            renderHashCode()
+            write("")
+            renderToString()
+            write("")
+            renderCopy()
+            write("")
+            renderBuilder()
+        }
+    }
+
+    private fun renderFields() {
+        params.forEach {
+            val initialValueExpr = if (it.isRequired) {
+                """requireNotNull(builder.${it.name}) { "endpoint provider parameter #${it.name} is required" }"""
+            } else {
+                "builder.${it.name}"
+            }
+
+            writer.ensureSuppressDeprecation(it)
+            it.renderDeclaration(writer, initialValueExpr)
+            writer.write("")
+        }
+    }
+
+    private fun renderDocumentation() {
+        writer.dokka {
+            write("The set of values necessary for endpoint resolution.")
+        }
+    }
+
+    private fun renderCompanionObject() {
+        writer.withBlock("public companion object {", "}") {
+            write("public operator fun invoke(block: Builder.() -> Unit): #L = Builder().apply(block).build()", CLASS_NAME)
+        }
+    }
+
+    private fun renderEquals() {
+        writer.withBlock("public override fun equals(other: Any?): Boolean {", "}") {
+            write("if (this === other) return true")
+            write("if (other !is #L) return false", CLASS_NAME)
+            params.forEach {
+                ensureSuppressDeprecation(it)
+                write("if (this.#1L != other.#1L) return false", it.name)
+            }
+            write("return true")
+        }
+    }
+
+    private fun renderHashCode() {
+        writer.withBlock("public override fun hashCode(): Int {", "}") {
+            if (params.isEmpty()) {
+                write("return this::class.hashCode()")
+                return@withBlock
+            }
+
+            ensureSuppressDeprecation(params[0])
+            write("var result = #L?.hashCode() ?: 0", params[0].name)
+            params.drop(1).forEach {
+                ensureSuppressDeprecation(it)
+                write("result = 31 * result + (#L?.hashCode() ?: 0)", it.name)
+            }
+            write("return result")
+        }
+    }
+
+    private fun renderToString() {
+        writer.withBlock("public override fun toString(): String = buildString {", "}") {
+            write("append(\"#L(\")", CLASS_NAME)
+            params.forEachIndexed { index, it ->
+                ensureSuppressDeprecation(it)
+                write("""append("#1L=$#1L#2L")""", it.name, if (index < params.size - 1) "," else ")")
+            }
+        }
+    }
+
+    private fun renderCopy() {
+        writer.withBlock("public fun copy(block: Builder.() -> Unit = {}): #L {", "}", CLASS_NAME) {
+            withBlock("return Builder().apply {", "}") {
+                params.forEach {
+                    ensureSuppressDeprecation(it)
+                    write("#1L = this@#2L.#1L", it.name, CLASS_NAME)
+                }
+                write("block()")
+            }
+            write(".build()")
+        }
+    }
+
+    private fun renderBuilder() {
+        writer.withBlock("public class Builder internal constructor() {", "}") {
+            params.forEach {
+                it.renderDeclaration(writer, it.defaultLiteral, isMutable = true)
+            }
+            write("")
+            write("internal fun build(): #1L = #1L(this)", CLASS_NAME)
+        }
+    }
+}
+
+// kotlin-mapped representation of an endpoint parameter
+private data class KotlinEndpointParameter(
+    val name: String,
+    val type: Symbol,
+    val isRequired: Boolean,
+    // All endpoint params are nullable. Standard symbol rendering suppresses the default value for boxed types, but
+    // endpoint params do not, so we must store the default ourselves to render.
+    val defaultLiteral: String,
+    val documentation: String?,
+    val deprecated: Parameter.Deprecated?,
+)
+
+private fun KotlinEndpointParameter.renderDeclaration(writer: KotlinWriter, initialValueLiteral: String, isMutable: Boolean = false) {
+    documentation?.let {
+        writer.dokka { write("#L", it) }
+    }
+    deprecated?.run { writeKotlinAnnotation(writer) }
+    writer.write("public #L #L: #P = #L", if (isMutable) "var" else "val", name, type, initialValueLiteral)
+    writer.write("")
+}
+
+private fun Identifier.toKotlin(): String =
+    asString().replaceFirstChar(Char::lowercase)
+
+private fun ParameterType.toSymbol(): Symbol =
+    when (this) {
+        ParameterType.STRING -> KotlinTypes.String
+        ParameterType.BOOLEAN -> KotlinTypes.Boolean
+    }
+        .toBuilder()
+        .boxed()
+        .build()
+
+private fun Value.toKotlinLiteral(): String =
+    when (this) {
+        is Value.Str -> expectString().doubleQuote()
+        is Value.Bool -> if (expectBool()) "true" else "false"
+        else -> throw IllegalArgumentException("unrecognized parameter value type")
+    }
+
+private fun Parameter.Deprecated.writeKotlinAnnotation(writer: KotlinWriter) =
+    writer.write("@Deprecated(#S)", message ?: DEFAULT_DEPRECATED_MESSAGE)
+
+private fun KotlinWriter.ensureSuppressDeprecation(param: KotlinEndpointParameter) =
+    param.deprecated?.let { write("@Suppress(\"DEPRECATION\")") }
---
+++ smithy-kotlin-codegen/src/test/kotlin/software/amazon/smithy/kotlin/codegen/rendering/endpoints/EndpointParametersGeneratorTest.kt
@@ -0,0 +1,310 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package software.amazon.smithy.kotlin.codegen.rendering.endpoints
+
+import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
+import software.amazon.smithy.kotlin.codegen.test.TestModelDefault
+import software.amazon.smithy.kotlin.codegen.test.assertBalancedBracesAndParens
+import software.amazon.smithy.kotlin.codegen.test.formatForTest
+import software.amazon.smithy.kotlin.codegen.test.shouldContainOnlyOnceWithDiff
+import software.amazon.smithy.model.node.Node
+import software.amazon.smithy.rulesengine.language.EndpointRuleset
+import kotlin.test.*
+
+class EndpointParametersGeneratorTest {
+    private val generatedClass: String
+
+    init {
+        val rules = createTestRulesetParams(
+            """
+                {
+                    "DocumentedField": {
+                        "type": "String",
+                        "required": false,
+                        "documentation": "A documented field. Has \"a quoted phrase\"."
+                    },
+                    "StringField": {
+                        "type": "String",
+                        "required": false
+                    },
+                    "BooleanField": {
+                        "type": "Boolean",
+                        "required": false
+                    },
+                    "RequiredStringField": {
+                        "type": "String",
+                        "required": true
+                    },
+                    "RequiredBooleanField": {
+                        "type": "Boolean",
+                        "required": true
+                    },
+                    "DefaultedStringField": {
+                        "type": "String",
+                        "required": true,
+                        "builtIn": "SDK::DefaultedStringField",
+                        "default": "default_string"
+                    },
+                    "DefaultedBooleanField": {
+                        "type": "Boolean",
+                        "required": true,
+                        "builtIn": "SDK::DefaultedBooleanField",
+                        "default": true
+                    },
+                    "DeprecatedField": {
+                        "type": "String",
+                        "required": false,
+                        "deprecated": {}
+                    },
+                    "DeprecatedMessageField": {
+                        "type": "String",
+                        "required": false,
+                        "deprecated": {
+                            "message": "arbitrary message about deprecation, has \"a quoted phrase\""
+                        }
+                    },
+                    "DocumentedDeprecatedField": {
+                        "type": "String",
+                        "required": false,
+                        "documentation": "description about a field but it's deprecated",
+                        "deprecated": {
+                            "message": "it's deprecated"
+                        }
+                    }
+                }
+            """,
+        )
+
+        val writer = KotlinWriter(TestModelDefault.NAMESPACE)
+        EndpointParametersGenerator(writer, rules).render()
+
+        generatedClass = writer.toString()
+    }
+
+    private fun createTestRulesetParams(paramsJson: String = "{}"): EndpointRuleset =
+        EndpointRuleset.fromNode(
+            Node.parse(
+                """
+                {
+                    "version": "1.1",
+                    "parameters": $paramsJson,
+                    "rules": []
+                }
+            """,
+            ),
+        )
+
+    @Test
+    fun testPackageDecl() {
+        assertContains(generatedClass, "package ${TestModelDefault.NAMESPACE}")
+    }
+
+    @Test
+    fun testBalancedSyntax() {
+        generatedClass.assertBalancedBracesAndParens()
+    }
+
+    @Test
+    fun testDocumentation() {
+        val expected = """
+            /**
+             * The set of values necessary for endpoint resolution.
+             */
+        """.formatForTest(indent = "")
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+
+    @Test
+    fun testConstructor() {
+        val expected = """
+            public class EndpointParameters private constructor(builder: Builder)
+        """.formatForTest(indent = "")
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+
+    @Test
+    fun testFields() {
+        val expected = """
+            public val booleanField: Boolean? = builder.booleanField
+         
+            public val defaultedBooleanField: Boolean? = requireNotNull(builder.defaultedBooleanField) { "endpoint provider parameter #defaultedBooleanField is required" }
+         
+            public val defaultedStringField: String? = requireNotNull(builder.defaultedStringField) { "endpoint provider parameter #defaultedStringField is required" }
+         
+            @Suppress("DEPRECATION")
+            @Deprecated("This field is deprecated and no longer recommended for use.")
+            public val deprecatedField: String? = builder.deprecatedField
+         
+            @Suppress("DEPRECATION")
+            @Deprecated("arbitrary message about deprecation, has \"a quoted phrase\"")
+            public val deprecatedMessageField: String? = builder.deprecatedMessageField
+         
+            @Suppress("DEPRECATION")
+            /**
+             * description about a field but it's deprecated
+             */
+            @Deprecated("it's deprecated")
+            public val documentedDeprecatedField: String? = builder.documentedDeprecatedField
+         
+            /**
+             * A documented field. Has "a quoted phrase".
+             */
+            public val documentedField: String? = builder.documentedField
+         
+            public val requiredBooleanField: Boolean? = requireNotNull(builder.requiredBooleanField) { "endpoint provider parameter #requiredBooleanField is required" }
+         
+            public val requiredStringField: String? = requireNotNull(builder.requiredStringField) { "endpoint provider parameter #requiredStringField is required" }
+         
+            public val stringField: String? = builder.stringField
+        """.formatForTest()
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+
+    @Test
+    fun testCompanionObject() {
+        val expected = """
+            public companion object {
+                public operator fun invoke(block: Builder.() -> Unit): EndpointParameters = Builder().apply(block).build()
+            }
+        """.formatForTest()
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+
+    @Test
+    fun testEquals() {
+        val expected = """
+            public override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (other !is EndpointParameters) return false
+                if (this.booleanField != other.booleanField) return false
+                if (this.defaultedBooleanField != other.defaultedBooleanField) return false
+                if (this.defaultedStringField != other.defaultedStringField) return false
+                @Suppress("DEPRECATION")
+                if (this.deprecatedField != other.deprecatedField) return false
+                @Suppress("DEPRECATION")
+                if (this.deprecatedMessageField != other.deprecatedMessageField) return false
+                @Suppress("DEPRECATION")
+                if (this.documentedDeprecatedField != other.documentedDeprecatedField) return false
+                if (this.documentedField != other.documentedField) return false
+                if (this.requiredBooleanField != other.requiredBooleanField) return false
+                if (this.requiredStringField != other.requiredStringField) return false
+                if (this.stringField != other.stringField) return false
+                return true
+            }
+        """.formatForTest()
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+
+    @Test
+    fun testHashCode() {
+        val expected = """
+            public override fun hashCode(): Int {
+                var result = booleanField?.hashCode() ?: 0
+                result = 31 * result + (defaultedBooleanField?.hashCode() ?: 0)
+                result = 31 * result + (defaultedStringField?.hashCode() ?: 0)
+                @Suppress("DEPRECATION")
+                result = 31 * result + (deprecatedField?.hashCode() ?: 0)
+                @Suppress("DEPRECATION")
+                result = 31 * result + (deprecatedMessageField?.hashCode() ?: 0)
+                @Suppress("DEPRECATION")
+                result = 31 * result + (documentedDeprecatedField?.hashCode() ?: 0)
+                result = 31 * result + (documentedField?.hashCode() ?: 0)
+                result = 31 * result + (requiredBooleanField?.hashCode() ?: 0)
+                result = 31 * result + (requiredStringField?.hashCode() ?: 0)
+                result = 31 * result + (stringField?.hashCode() ?: 0)
+                return result
+            }
+        """.formatForTest()
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+
+    @Test
+    fun testToString() {
+        val expected = """
+            public override fun toString(): String = buildString {
+                append("EndpointParameters(")
+                append("booleanField=${'$'}booleanField,")
+                append("defaultedBooleanField=${'$'}defaultedBooleanField,")
+                append("defaultedStringField=${'$'}defaultedStringField,")
+                @Suppress("DEPRECATION")
+                append("deprecatedField=${'$'}deprecatedField,")
+                @Suppress("DEPRECATION")
+                append("deprecatedMessageField=${'$'}deprecatedMessageField,")
+                @Suppress("DEPRECATION")
+                append("documentedDeprecatedField=${'$'}documentedDeprecatedField,")
+                append("documentedField=${'$'}documentedField,")
+                append("requiredBooleanField=${'$'}requiredBooleanField,")
+                append("requiredStringField=${'$'}requiredStringField,")
+                append("stringField=${'$'}stringField)")
+            }
+        """.formatForTest()
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+
+    @Test
+    fun testCopy() {
+        val expected = """
+            public fun copy(block: Builder.() -> Unit = {}): EndpointParameters {
+                return Builder().apply {
+                    booleanField = this@EndpointParameters.booleanField
+                    defaultedBooleanField = this@EndpointParameters.defaultedBooleanField
+                    defaultedStringField = this@EndpointParameters.defaultedStringField
+                    @Suppress("DEPRECATION")
+                    deprecatedField = this@EndpointParameters.deprecatedField
+                    @Suppress("DEPRECATION")
+                    deprecatedMessageField = this@EndpointParameters.deprecatedMessageField
+                    @Suppress("DEPRECATION")
+                    documentedDeprecatedField = this@EndpointParameters.documentedDeprecatedField
+                    documentedField = this@EndpointParameters.documentedField
+                    requiredBooleanField = this@EndpointParameters.requiredBooleanField
+                    requiredStringField = this@EndpointParameters.requiredStringField
+                    stringField = this@EndpointParameters.stringField
+                    block()
+                }
+                .build()
+            }
+        """.formatForTest()
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+
+    @Test
+    fun testBuilder() {
+        val expected = """
+            public class Builder internal constructor() {
+                public var booleanField: Boolean? = null
+         
+                public var defaultedBooleanField: Boolean? = true
+         
+                public var defaultedStringField: String? = "default_string"
+         
+                @Deprecated("This field is deprecated and no longer recommended for use.")
+                public var deprecatedField: String? = null
+         
+                @Deprecated("arbitrary message about deprecation, has \"a quoted phrase\"")
+                public var deprecatedMessageField: String? = null
+         
+                /**
+                 * description about a field but it's deprecated
+                 */
+                @Deprecated("it's deprecated")
+                public var documentedDeprecatedField: String? = null
+         
+                /**
+                 * A documented field. Has "a quoted phrase".
+                 */
+                public var documentedField: String? = null
+         
+                public var requiredBooleanField: Boolean? = null
+         
+                public var requiredStringField: String? = null
+         
+                public var stringField: String? = null
+         
+                internal fun build(): EndpointParameters = EndpointParameters(this)
+            }
+        """.formatForTest()
+        generatedClass.shouldContainOnlyOnceWithDiff(expected)
+    }
+}
