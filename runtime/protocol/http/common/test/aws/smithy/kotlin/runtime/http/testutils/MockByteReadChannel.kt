dda4617a3cced6b556464c2853313c41c8863b75
Ian Smith Botsford <issmith@amazon.com>
2022-09-28 23:00:24 +0000

    add missing copyright headers

--- runtime/protocol/http/common/test/aws/smithy/kotlin/runtime/http/testutils/MockByteReadChannel.kt
+++ runtime/protocol/http/common/test/aws/smithy/kotlin/runtime/http/testutils/MockByteReadChannel.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package aws.smithy.kotlin.runtime.http.testutils
 
 import aws.smithy.kotlin.runtime.io.SdkByteReadChannel
