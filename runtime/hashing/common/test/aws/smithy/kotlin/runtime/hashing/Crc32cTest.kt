d3ee81dceab52f9c51f8d4101c1ccfe537b185da
Matas Lauzadis <lauzmata@amazon.com>
2022-10-28 15:11:55 -0500

    Add more tests for CRC32 family

--- runtime/hashing/common/test/aws/smithy/kotlin/runtime/hashing/Crc32cTest.kt
+++ runtime/hashing/common/test/aws/smithy/kotlin/runtime/hashing/Crc32cTest.kt
@@ -4,6 +4,7 @@
  */
 package aws.smithy.kotlin.runtime.hashing
 
+import aws.smithy.kotlin.runtime.util.encodeBase64String
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -11,5 +12,52 @@ class Crc32cTest {
     @Test
     fun testCrc32c() {
         assertEquals(224353407U, "foobar".encodeToByteArray().crc32c())
+    }
+
+    @Test
+    fun testReset() {
+        val crc = Crc32c()
+        val input = "foobar"
+
+        crc.update(input.encodeToByteArray(), 0, input.length)
+        assertEquals(224353407U, crc.digestValue())
+
+        crc.reset()
+        assertEquals(0U, crc.digestValue())  // contents of crc should be zeroed out
+    }
+
+    @Test
+    fun testChainedChecksum() {
+        // run the checksum on both halves of the input with separate calls
+        val crc = Crc32c()
+        val input = "foobar"
+
+        crc.update(input.encodeToByteArray(), 0, input.length / 2)
+        assertEquals(3485773341U, crc.digestValue())  // checksum of "foo"
+
+        crc.update(input.encodeToByteArray(), (input.length - input.length / 2), input.length / 2)
+        assertEquals(224353407U, crc.digestValue()) // checksum of "foobar"
+    }
+
+    @Test
+    fun testBase64EncodedChecksumValue() {
+        val crc = Crc32c()
+        val input = "foobar"
+
+        crc.update(input.encodeToByteArray(), 0, input.length)
+        assertEquals(224353407U, crc.digestValue()) // checksum of "foobar"
+        assertEquals("DV9cfw==", crc.digest().encodeBase64String())
+    }
+
+    @Test
+    fun testDigestResetsValue() {
+        val crc = Crc32c()
+        val input = "foobar"
+
+        crc.update(input.encodeToByteArray(), 0, input.length)
+        assertEquals(224353407U, crc.digestValue()) // checksum of "foobar"
+
+        crc.digest()
+        assertEquals(0U, crc.digestValue()) // checksum should be reset
     }
 }
