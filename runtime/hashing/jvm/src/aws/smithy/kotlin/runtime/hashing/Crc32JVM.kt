cc622d8e679ada21c6e72f8d9d3aea10b289bb37
Matas Lauzadis <lauzmata@amazon.com>
2022-10-31 17:27:49 -0500

    move digest function definition to base class

--- runtime/hashing/jvm/src/aws/smithy/kotlin/runtime/hashing/Crc32JVM.kt
+++ runtime/hashing/jvm/src/aws/smithy/kotlin/runtime/hashing/Crc32JVM.kt
@@ -11,17 +11,6 @@ public actual class Crc32 : Crc32Base() {
 
     override fun update(input: ByteArray, offset: Int, length: Int): Unit = md.update(input, offset, length)
 
-    override fun digest(): ByteArray {
-        val x = digestValue()
-        reset()
-        return byteArrayOf(
-            ((x shr 24) and 0xffu).toByte(),
-            ((x shr 16) and 0xffu).toByte(),
-            ((x shr 8) and 0xffu).toByte(),
-            (x and 0xffu).toByte(),
-        )
-    }
-
     override fun digestValue(): UInt = md.value.toUInt()
 
     override fun reset(): Unit = md.reset()
