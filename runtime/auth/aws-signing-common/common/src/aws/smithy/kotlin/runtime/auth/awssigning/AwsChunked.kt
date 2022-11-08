feac3edd31e2b77fca50b40849da0cbf1f6c189b
Matas Lauzadis <lauzmata@amazon.com>
2022-11-08 14:54:55 -0600

    Add AwsChunked and AwsChunkedJVM

---
+++ runtime/auth/aws-signing-common/common/src/aws/smithy/kotlin/runtime/auth/awssigning/AwsChunked.kt
@@ -0,0 +1,14 @@
+package aws.smithy.kotlin.runtime.auth.awssigning
+
+import aws.smithy.kotlin.runtime.io.SdkByteReadChannel
+
+/**
+ * AwsChunked Encoding
+ * not thread-safe! do not use multiple executors to read from the same AwsChunked object
+ */
+internal expect class AwsChunked constructor(
+    chan: SdkByteReadChannel,
+    signer: AwsSigner,
+    signingConfig: AwsSigningConfig,
+    previousSignature: ByteArray
+): AbstractAwsChunked
---
+++ runtime/auth/aws-signing-common/jvm/src/aws/smithy/kotlin/runtime/auth/awssigning/AwsChunkedJVM.kt
@@ -0,0 +1,36 @@
+package aws.smithy.kotlin.runtime.auth.awssigning
+
+import aws.smithy.kotlin.runtime.io.SdkByteReadChannel
+import java.nio.ByteBuffer
+
+internal actual class AwsChunked constructor(
+    chan: SdkByteReadChannel,
+    signer: AwsSigner,
+    signingConfig: AwsSigningConfig,
+    previousSignature: ByteArray
+): AbstractAwsChunked(chan, signer, signingConfig, previousSignature) {
+
+    override suspend fun readAvailable(sink: ByteBuffer): Int {
+        if (chunk == null || chunkOffset >= chunk!!.size) { chunk = getNextChunk() }
+
+        var bytesWritten = 0
+        while (chunkOffset < chunk!!.size) {
+            val numBytesToWrite = chunk!!.size - chunkOffset
+
+            val bytes = chunk!!.slice(chunkOffset .. chunkOffset + numBytesToWrite).toByteArray()
+
+            sink.put(bytes)
+
+            bytesWritten += numBytesToWrite
+            chunkOffset += numBytesToWrite
+
+            sink.position(bytesWritten)
+
+            // if we've exhausted the current chunk, exit without suspending for a new one
+            if (chunk == null || chunkOffset >= chunk!!.size) { break }
+        }
+
+        sink.flip()
+        return bytesWritten
+    }
+}
