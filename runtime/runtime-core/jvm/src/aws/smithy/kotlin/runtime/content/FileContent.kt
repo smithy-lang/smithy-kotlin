3adc7e13ca982725d8bc059db1f1e3d9cde7cd4a
Aaron J Todd <todaaron@amazon.com>
2022-11-10 14:54:32 -0500

    reimplement file channel support

--- runtime/runtime-core/jvm/src/aws/smithy/kotlin/runtime/content/FileContent.kt
+++ runtime/runtime-core/jvm/src/aws/smithy/kotlin/runtime/content/FileContent.kt
@@ -5,8 +5,10 @@
 
 package aws.smithy.kotlin.runtime.content
 
-import aws.smithy.kotlin.runtime.io.SdkByteReadChannel
+import aws.smithy.kotlin.runtime.io.*
+import kotlinx.coroutines.*
 import java.io.File
+import kotlin.io.use
 
 /**
  * ByteStream backed by a local [file]
@@ -20,6 +22,43 @@ public class FileContent(
     override val contentLength: Long
         get() = endInclusive - start + 1
 
-    // FIXME - is this what we want? Or can we directly consume e.g. `SdkSource` as a bytestream variant?
-    override fun newReader(): SdkByteReadChannel = TODO("needs reimplemented")
+    override fun newReader(): SdkByteReadChannel {
+        val ch = FileChannel()
+        val job = CoroutineScope(Dispatchers.IO + CoroutineName("file-reader")).launch {
+            file.source(start, endInclusive).use {
+                ch.writeAll(it)
+            }
+        }
+
+        job.invokeOnCompletion { cause ->
+            ch.close(cause)
+        }
+
+        // attach the job and tie the lifetimes of the coroutine and channel together
+        ch.job = job
+
+        return ch
+    }
+}
+
+private class FileChannel(
+    private val delegate: SdkByteChannel = SdkByteChannel(true),
+) : SdkByteChannel by delegate {
+    var job: Job? = null
+
+    override fun cancel(cause: Throwable?): Boolean {
+        job?.cancel(CancellationException("channel was cancelled", cause))
+        return delegate.cancel(cause)
+    }
+
+    override fun close(cause: Throwable?): Boolean {
+        if (cause != null) {
+            job?.cancel(CancellationException("channel was closed with cause", cause))
+        }
+        return delegate.close(cause)
+    }
+
+    override fun close() {
+        delegate.close()
+    }
 }
