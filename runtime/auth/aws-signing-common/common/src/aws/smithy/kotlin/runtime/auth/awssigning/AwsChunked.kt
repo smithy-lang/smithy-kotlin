b279a6f5304bf92b80ff84617c704e1ee9baf9cc
Matas Lauzadis <lauzmata@amazon.com>
2022-11-10 14:06:00 -0600

    Add trailing headers and documentation link

--- runtime/auth/aws-signing-common/common/src/aws/smithy/kotlin/runtime/auth/awssigning/AwsChunked.kt
+++ runtime/auth/aws-signing-common/common/src/aws/smithy/kotlin/runtime/auth/awssigning/AwsChunked.kt
@@ -5,19 +5,24 @@
 
 package aws.smithy.kotlin.runtime.auth.awssigning
 
+import aws.smithy.kotlin.runtime.http.Headers
 import aws.smithy.kotlin.runtime.io.SdkByteReadChannel
 
 /**
- * AwsChunked Encoding
- * Operations on this class can not be invoked concurrently.
+ * aws-chunked content encoding. Operations on this class can not be invoked concurrently.
+ * This class wraps an SdkByteReadChannel. When reads are performed on this class, it will read the wrapped data
+ * and return it in aws-chunked content encoding.
+ * @see <a href="https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-streaming.html">SigV4 Streaming</a>
  * @param chan the underlying [SdkByteReadChannel] which will have its data encoded in aws-chunked format
  * @param signer the signer to use to sign chunks and (optionally) chunk trailer
  * @param signingConfig the config to use for signing
- * @previousSignature the previous signature to use for signing. in most cases, this should be the seed signature
+ * @param previousSignature the previous signature to use for signing. in most cases, this should be the seed signature
+ * @param trailingHeaders the optional trailing headers to include in the final chunk
  */
 internal expect class AwsChunked internal constructor(
     chan: SdkByteReadChannel,
     signer: AwsSigner,
     signingConfig: AwsSigningConfig,
     previousSignature: ByteArray,
+    trailingHeaders: Headers = Headers.Empty
 ) : AbstractAwsChunked
