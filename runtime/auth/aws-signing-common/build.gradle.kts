f08527b4c90e320d6c9f56e9a5f9bc0e6d9847de
Matas Lauzadis <lauzmata@amazon.com>
2022-11-10 17:16:53 -0600

    remove duplicated test dependencies

--- runtime/auth/aws-signing-common/build.gradle.kts
+++ runtime/auth/aws-signing-common/build.gradle.kts
@@ -18,13 +18,6 @@ kotlin {
                 implementation(project(":runtime:logging"))
             }
         }
-        commonTest {
-            dependencies {
-                implementation(project(":runtime:auth:aws-signing-default"))
-                implementation(project(":runtime:auth:aws-signing-tests"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
-            }
-        }
 
         commonTest {
             dependencies {
