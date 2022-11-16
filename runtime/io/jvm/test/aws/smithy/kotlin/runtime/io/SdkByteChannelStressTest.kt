0a0766b38587ffb811d37d2d9a6a61f3b395e2e5
Aaron Todd <aajtodd@users.noreply.github.com>
2022-11-16 10:43:32 -0500

    refactor(rt)!: simplify SdkByteChannel interface (#733)
    Refactor SdkByteChannel to be based on the new SdkBuffer type. Moving data between buffers is cheap (especially in bulk) so this type decouples a producer and consumer with minimal overhead. Ktor based channels did some clever things internally but in many common cases it still required an explicit copy for each read/write method. Having a single read/write method to implement makes layering/wrapping a channel much easier than before (similar to how easy it is to layer okio Source/Sink types).
    
    * Removes ktor based channel dependency
    * Updates SdkByteReadChannel/SdkByteWriteChannel to have a single read/write method based on SdkBuffer. This makes it effectively an async Source/Sink or an async Pipe depending on how you want to look at it.
    * Adds benchmarks comparing the new channel to ktor based channel.
        * NOTE: These benchmarks are not really an apples-to-apples comparison and thus are not really fair. They are however an idiomatic usage of the channel comparison and should be looked at through that lens.
    
    BREAKING CHANGE: `SdkByteReadChannel` and `SdkByteWriteChannel` APIs have changed.

---
+++ runtime/io/jvm/test/aws/smithy/kotlin/runtime/io/SdkByteChannelStressTest.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package aws.smithy.kotlin.runtime.io
+
+import kotlinx.coroutines.*
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.seconds
+
+class SdkByteChannelStressTest {
+
+    @Test
+    fun testWriteByteRaceCondition() = runBlocking {
+        testReadWriteRaceCondition { it.writeByte(1) }
+    }
+
+    @Test
+    fun testWriteIntRaceCondition() {
+        testReadWriteRaceCondition { it.writeInt(1) }
+    }
+
+    val testBytes = ByteArray(1024 + 97) { it.toByte() }
+
+    internal class TestSource(
+        private val bytes: ByteArray,
+    ) : SdkSource {
+        private var closed = false
+        override fun close() { closed = true }
+        override fun read(sink: SdkBuffer, limit: Long): Long {
+            if (closed) return -1L
+            sink.write(bytes, 0, bytes.size)
+            close()
+            return bytes.size.toLong()
+        }
+    }
+
+    @Test
+    fun testWriteAllSourceCondition() {
+        testReadWriteRaceCondition {
+            val source = TestSource(testBytes)
+            it.writeAll(source)
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun testReadWriteRaceCondition(writer: (SdkBuffer) -> Unit): Unit = runBlocking {
+        val channel = SdkByteChannel(false)
+
+        val writeJob = GlobalScope.async {
+            try {
+                val sink = SdkBuffer()
+                repeat(1_000_000) {
+                    writer(sink)
+                    channel.write(sink)
+                    channel.flush()
+                }
+                channel.close()
+            } catch (ex: Exception) {
+                channel.close(ex)
+                throw ex
+            }
+        }
+
+        val readJob = GlobalScope.async {
+            channel.readAll(SdkSink.blackhole())
+        }
+
+        withTimeout(60.seconds) {
+            writeJob.await()
+            readJob.await()
+        }
+    }
+
+    @Test
+    fun testReadAllSinkWriteAllSource() = runBlocking {
+        val ch = SdkByteChannel(false)
+        val source = TestSource(testBytes)
+        val wc = ch.writeAll(source)
+        ch.close()
+        assertEquals(testBytes.size.toLong(), wc)
+
+        val sink = SdkBuffer()
+        val rc = ch.readAll(sink)
+        assertEquals(testBytes.size.toLong(), rc)
+        assertEquals(rc, sink.size)
+
+        assertContentEquals(testBytes, sink.readByteArray())
+    }
+}
