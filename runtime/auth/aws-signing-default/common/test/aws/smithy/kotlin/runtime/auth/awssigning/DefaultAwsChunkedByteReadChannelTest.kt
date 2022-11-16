c9287e02bbbb5a401b3ca72367e17e6d8fee76d6
Matas Lauzadis <lauzmata@amazon.com>
2022-11-16 10:29:23 -0600

    add `Default` prefix to class name

--- runtime/auth/aws-signing-default/common/test/aws/smithy/kotlin/runtime/auth/awssigning/DefaultAwsChunkedByteReadChannelTest.kt
+++ runtime/auth/aws-signing-default/common/test/aws/smithy/kotlin/runtime/auth/awssigning/DefaultAwsChunkedByteReadChannelTest.kt
@@ -2,6 +2,6 @@ package aws.smithy.kotlin.runtime.auth.awssigning
 
 import aws.smithy.kotlin.runtime.auth.awssigning.tests.AwsChunkedByteReadChannelTestBase
 
-class AwsChunkedByteReadChannelTest : AwsChunkedByteReadChannelTestBase() {
+class DefaultAwsChunkedByteReadChannelTest : AwsChunkedByteReadChannelTestBase() {
     override val signer: AwsSigner = DefaultAwsSigner
 }
