f3083a939242c35d1bd3288d2f33c0568253c6e2
Aaron J Todd <todaaron@amazon.com>
2022-12-08 12:12:52 -0500

    working example

--- runtime/protocol/http-client-engines/http-client-engine-okhttp/build.gradle.kts
+++ runtime/protocol/http-client-engines/http-client-engine-okhttp/build.gradle.kts
@@ -4,6 +4,7 @@
  */
 
 plugins {
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -15,6 +16,8 @@ val coroutinesVersion: String by project
 val okHttpVersion: String by project
 
 kotlin {
+    jvm()
+
     sourceSets {
         commonMain {
             dependencies {
@@ -24,7 +27,6 @@ kotlin {
 
                 implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
                 implementation("com.squareup.okhttp3:okhttp-coroutines:$okHttpVersion")
-
             }
         }
         commonTest {
@@ -46,9 +48,13 @@ kotlin {
     }
 }
 
-tasks.register<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+val shadowTask = tasks.register<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
     group = "shadow"
-    archiveClassifier.set("all")
+    // strip the default '-all' classifier, we won't need it since we publish this as a separate artifact
+    // to get the right dependencies in the generated pom file (if they shared GAV coordinates they
+    // would share the same POM file with the shaded deps in it)
+    archiveClassifier.set("")
+    archiveAppendix.set("shaded")
     val main by kotlin.jvm().compilations
     from(main.output)
     configurations += main.compileDependencyFiles as Configuration
@@ -61,4 +67,76 @@ tasks.register<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shad
     }
 
     relocate("okhttp3", "aws.smithy.kotlin.shaded.okhttp3")
+}
+
+tasks.assemble.configure {
+    dependsOn(shadowTask)
+}
+
+val shadedConfig = configurations.create("shaded") {
+    attributes {
+        attribute(
+            Bundling.BUNDLING_ATTRIBUTE,
+            objects.named(Bundling.SHADOWED)
+        )
+        attribute(
+            LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE,
+            objects.named(LibraryElements.JAR)
+        )
+        attribute(
+            Category.CATEGORY_ATTRIBUTE,
+            objects.named(Category.LIBRARY)
+        )
+        attribute(
+            Usage.USAGE_ATTRIBUTE,
+            objects.named(Usage.JAVA_RUNTIME)
+        )
+    }
+    extendsFrom(configurations.getByName("jvmRuntimeClasspath"))
+
+    // FIXME - can we get this configuration dependency set correct such that we don't have to filter it when generating the POM?
+
+    // don't export as an outgoing variant
+    isCanBeConsumed = false
+    isCanBeResolved = false
+
+}
+
+artifacts {
+    add(shadedConfig.name, shadowTask)
+}
+
+publishing {
+    publications {
+
+        create<MavenPublication>("shadow") {
+            // There are some open bugs in shadow plugin that result in either broken OR
+            // incorrect POMS that include the shaded deps:
+            // https://github.com/johnrengelman/shadow/issues/634
+            // https://github.com/johnrengelman/shadow/issues/807
+            //
+            // This prevents us from just doing something like:
+            // project.extensions.configure<com.github.jengelman.gradle.plugins.shadow.ShadowExtension>{
+            //     component(this@create)
+            // }
+
+            artifact(shadowTask)
+            artifactId = "$artifactId-shaded"
+            pom.packaging = "jar"
+
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+                shadedConfig.allDependencies.forEach {
+                    if ((it is ProjectDependency) || (it !is SelfResolvingDependency) && it.group != "com.squareup.okhttp3") {
+                        val dependencyNode = dependenciesNode.appendNode("dependency")
+                        dependencyNode.appendNode("groupId", it.group)
+                        dependencyNode.appendNode("artifactId", it.name)
+                        dependencyNode.appendNode("version", it.version)
+                        // FIXME - some of these are actually `compile` scope in the unshaded version...
+                        dependencyNode.appendNode("scope", "runtime")
+                    }
+                }
+            }
+        }
+    }
 }
