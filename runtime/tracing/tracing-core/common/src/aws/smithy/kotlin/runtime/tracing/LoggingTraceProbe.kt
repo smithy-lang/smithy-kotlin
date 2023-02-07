57dfad1cdafb0fbbcc86ed112143564fd5e4782d
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2023-02-07 07:39:16 -0800

    fix: stop logging "null" when exceptions are not present in trace events (#790)

--- runtime/tracing/tracing-core/common/src/aws/smithy/kotlin/runtime/tracing/LoggingTraceProbe.kt
+++ runtime/tracing/tracing-core/common/src/aws/smithy/kotlin/runtime/tracing/LoggingTraceProbe.kt
@@ -7,36 +7,41 @@ package aws.smithy.kotlin.runtime.tracing
 import mu.KLogger
 import mu.KotlinLogging
 
+private fun EventLevel.loggerMethod(): (KLogger, () -> Any?) -> Unit = when (this) {
+    EventLevel.Fatal,
+    EventLevel.Error,
+    -> KLogger::error
+    EventLevel.Warning -> KLogger::warn
+    EventLevel.Info -> KLogger::info
+    EventLevel.Debug -> KLogger::debug
+    EventLevel.Trace -> KLogger::trace
+}
+
+private fun EventLevel.throwableLoggerMethod(): (KLogger, Throwable, () -> Any?) -> Unit = when (this) {
+    EventLevel.Fatal,
+    EventLevel.Error,
+    -> KLogger::error
+    EventLevel.Warning -> KLogger::warn
+    EventLevel.Info -> KLogger::info
+    EventLevel.Debug -> KLogger::debug
+    EventLevel.Trace -> KLogger::trace
+}
+
 /**
  * A [TraceProbe] that sends logs events to downstream logging libraries (e.g., Slf4j on JVM). Those downstream
  * libraries may require configuration before messages will actually appear in logs.
  */
 public object LoggingTraceProbe : TraceProbe {
-    private fun EventLevel.loggerMethod(): (KLogger, () -> Any?) -> Unit = when (this) {
-        EventLevel.Fatal,
-        EventLevel.Error,
-        -> KLogger::error
-        EventLevel.Warning -> KLogger::warn
-        EventLevel.Info -> KLogger::info
-        EventLevel.Debug -> KLogger::debug
-        EventLevel.Trace -> KLogger::trace
-    }
-
-    private fun log(spanId: String, event: TraceEvent) {
+    private fun log(spanId: String, event: TraceEvent, message: TraceEventData.Message) {
         val logger = KotlinLogging.logger(event.sourceComponent)
-        val method = event.level.loggerMethod()
-        method(logger) {
-            val message = event.data as TraceEventData.Message
-
-            val content = message.content()
-            val exception = message.exception?.let { ": $it" } ?: ""
-
-            "$spanId: $content$exception"
+        when (message.exception) {
+            null -> event.level.loggerMethod()(logger) { "$spanId: ${message.content()}" }
+            else -> event.level.throwableLoggerMethod()(logger, message.exception) { "$spanId: ${message.content()}" }
         }
     }
 
     override fun postEvent(span: TraceSpan, event: TraceEvent) {
-        if (event.data is TraceEventData.Message) log(span.hierarchicalId, event)
+        if (event.data is TraceEventData.Message) log(span.hierarchicalId, event, event.data)
     }
 
     override fun spanClosed(span: TraceSpan) { } // No action necessary
