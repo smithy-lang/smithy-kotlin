fba0fadf6d9cb5cc653630fdf8092b87bc84e95c
Matas Lauzadis <lauzmata@amazon.com>
2023-04-14 09:05:42 -0500

    Use `&&` for boolean operators and make `fromString` more readable

--- runtime/smithy-client/common/src/aws/smithy/kotlin/runtime/client/SdkLogMode.kt
+++ runtime/smithy-client/common/src/aws/smithy/kotlin/runtime/client/SdkLogMode.kt
@@ -55,7 +55,7 @@ public sealed class SdkLogMode(private val mask: Int) {
     public operator fun plus(mode: SdkLogMode): SdkLogMode = Composite(mask or mode.mask)
     public operator fun minus(mode: SdkLogMode): SdkLogMode = Composite(mask and mode.mask.inv())
 
-    public override fun equals(other: Any?): Boolean = (other is SdkLogMode) and (mask == (other as SdkLogMode).mask)
+    public override fun equals(other: Any?): Boolean = (other is SdkLogMode) && (mask == other.mask)
 
     /**
      * Test if a particular [SdkLogMode] is enabled
@@ -78,9 +78,14 @@ public sealed class SdkLogMode(private val mask: Int) {
          * @return the parsed SdkLogMode, or `null` if no log mode could be parsed
          */
         public fun fromString(string: String): SdkLogMode? =
-            string.trim().split("|").mapNotNull {
-                allModes().firstOrNull { sdkLogMode -> it.equals(sdkLogMode.toString(), ignoreCase = true) }
-            }.takeIf { it.isNotEmpty() }?.let { it.reduce { acc, sdkLogMode -> acc + sdkLogMode } }
+            string
+                .trim()
+                .split("|")
+                .mapNotNull { sdkLogModeString ->
+                    allModes().firstOrNull { sdkLogMode -> sdkLogModeString.equals(sdkLogMode.toString(), ignoreCase = true) }
+                }
+                .takeIf { it.isNotEmpty() }
+                ?.let { it.reduce { acc, sdkLogMode -> acc + sdkLogMode } }
     }
 
     override fun toString(): String =
