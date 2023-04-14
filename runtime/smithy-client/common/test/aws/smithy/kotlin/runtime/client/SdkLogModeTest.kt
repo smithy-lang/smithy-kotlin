b5ef9f781d327fb512b436dd2d1a92d294129a7d
Matas Lauzadis <lauzmata@amazon.com>
2023-04-14 11:50:37 -0500

    Update toString test

--- runtime/smithy-client/common/test/aws/smithy/kotlin/runtime/client/SdkLogModeTest.kt
+++ runtime/smithy-client/common/test/aws/smithy/kotlin/runtime/client/SdkLogModeTest.kt
@@ -32,7 +32,7 @@ class SdkLogModeTest {
     fun testToString() {
         val mode = SdkLogMode.allModes().reduce { acc, curr -> acc + curr }
         assertTrue { SdkLogMode.allModes().all { mode.isEnabled(it) } }
-        val expected = "SdkLogMode(LogRequest|LogRequestWithBody|LogResponse|LogResponseWithBody)"
+        val expected = "LogRequest|LogRequestWithBody|LogResponse|LogResponseWithBody"
         assertEquals(expected, mode.toString())
     }
 
