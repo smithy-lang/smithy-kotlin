0ed44825fe108e957776c003fd9972896ffa5edb
Aaron J Todd <todaaron@amazon.com>
2023-06-14 14:13:59 -0400

    fix logger and api dump

--- runtime/observability/telemetry-api/common/src/aws/smithy/kotlin/runtime/telemetry/logging/NoOpLogger.kt
+++ runtime/observability/telemetry-api/common/src/aws/smithy/kotlin/runtime/telemetry/logging/NoOpLogger.kt
@@ -13,44 +13,22 @@ internal object NoOpLoggerProvider : LoggerProvider {
 }
 
 private object NoOpLogger : Logger {
-    override fun trace(msg: () -> Any) {}
-
     override fun trace(t: Throwable?, msg: () -> Any) {}
-
-    override fun debug(msg: () -> Any) {}
-
     override fun debug(t: Throwable?, msg: () -> Any) {}
-
-    override fun info(msg: () -> Any) {}
-
     override fun info(t: Throwable?, msg: () -> Any) {}
-
-    override fun warn(msg: () -> Any) {}
-
     override fun warn(t: Throwable?, msg: () -> Any?) {}
-
-    override fun error(msg: () -> Any) {}
-
     override fun error(t: Throwable?, msg: () -> Any) {}
-
     override fun isEnabledFor(level: LogLevel): Boolean = false
     override fun logRecordBuilder(): LogRecordBuilder = NoOpLogRecordBuilder
 }
 
 private object NoOpLogRecordBuilder : LogRecordBuilder {
     override fun setLevel(level: LogLevel) {}
-
     override fun setTimestamp(ts: Instant) {}
-
     override fun setCause(ex: Throwable) {}
-
     override fun setMessage(message: String) {}
-
     override fun setMessage(message: () -> Any) {}
-
     override fun setAttribute(name: String, value: Any) {}
-
     override fun setAllAttributes(attributes: Attributes) {}
-
     override fun emit() {}
 }
