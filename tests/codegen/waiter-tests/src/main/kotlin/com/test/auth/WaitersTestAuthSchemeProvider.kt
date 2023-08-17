fc002c5029ac58b63b1b7f7c6ef0585331f03e7a
Aaron J Todd <todaaron@amazon.com>
2023-08-17 16:37:10 -0400

    lint

--- tests/codegen/waiter-tests/src/main/kotlin/com/test/auth/WaitersTestAuthSchemeProvider.kt
+++ tests/codegen/waiter-tests/src/main/kotlin/com/test/auth/WaitersTestAuthSchemeProvider.kt
@@ -1,7 +1,10 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package com.test.auth
 
 import aws.smithy.kotlin.runtime.auth.AuthSchemeProvider
-
 
 // Without a protocol generator there are certain components expected to exist that aren't getting generated.
 // Fill in the types manually so that the generated code compiles. None of these are needed for the tests at hand.
