bb57855ffcd930e5ad6a549c78f23c4c054aec39
Matas Lauzadis <lauzmata@amazon.com>
2023-10-06 15:35:19 -0400

    Refactor clock skew application

--- runtime/auth/http-auth-aws/common/src/aws/smithy/kotlin/runtime/http/auth/AwsHttpSigner.kt
+++ runtime/auth/http-auth-aws/common/src/aws/smithy/kotlin/runtime/http/auth/AwsHttpSigner.kt
@@ -15,6 +15,7 @@ import aws.smithy.kotlin.runtime.http.HttpBody
 import aws.smithy.kotlin.runtime.http.operation.HttpOperationContext
 import aws.smithy.kotlin.runtime.http.request.HttpRequest
 import aws.smithy.kotlin.runtime.http.request.HttpRequestBuilder
+import aws.smithy.kotlin.runtime.time.Instant
 import aws.smithy.kotlin.runtime.util.get
 import kotlin.time.Duration
 
@@ -124,7 +125,10 @@ public class AwsHttpSigner(private val config: Config) : HttpSigner {
             service = attributes.getOrNull(AwsSigningAttributes.SigningService) ?: checkNotNull(config.service)
             credentials = signingRequest.identity as Credentials
             algorithm = config.algorithm
-            signingDate = attributes.getOrNull(AwsSigningAttributes.SigningDate)
+
+            // apply clock skew if applicable
+            signingDate = (attributes.getOrNull(AwsSigningAttributes.SigningDate) ?: Instant.now()) +
+                (attributes.getOrNull(HttpOperationContext.ClockSkew) ?: Duration.ZERO)
 
             signatureType = config.signatureType
             omitSessionToken = config.omitSessionToken
@@ -152,15 +156,6 @@ public class AwsHttpSigner(private val config: Config) : HttpSigner {
                 // use the payload to compute the hash
                 else -> HashSpecification.CalculateFromPayload
             }
-        }
-
-        // Apply clock skew if necessary
-        signingConfig = if (attributes.contains(HttpOperationContext.ClockSkew)) {
-            val builder = signingConfig.toBuilder()
-            builder.signingDate = builder.signingDate?.plus(attributes[HttpOperationContext.ClockSkew])
-            builder.build()
-        } else {
-            signingConfig
         }
 
         if (signingConfig.useAwsChunkedEncoding) {
