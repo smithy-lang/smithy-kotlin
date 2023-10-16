4dc69c5caaef27f147e4baaead7ab7e9b4834cc8
natanfudge <natan.lifsiz@gmail.com>
2023-10-16 16:51:01 +0300

    Solve NoSuchMethodErrors in composite slf4j setups

--- runtime/observability/logging-slf4j2/jvm/src/aws/smithy/kotlin/runtime/telemetry/logging/slf4j/Slf4jLoggerProvider.kt
+++ runtime/observability/logging-slf4j2/jvm/src/aws/smithy/kotlin/runtime/telemetry/logging/slf4j/Slf4jLoggerProvider.kt
@@ -7,23 +7,31 @@ package aws.smithy.kotlin.runtime.telemetry.logging.slf4j
 
 import aws.smithy.kotlin.runtime.telemetry.logging.*
 import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
 
 /**
  * SLF4J based logger provider
  */
 public object Slf4jLoggerProvider : LoggerProvider {
+    private var useSlf4j2x: Boolean? = null
 
-    private val useSlf4j2x = try {
-        Class.forName("org.slf4j.spi.LoggingEventBuilder")
-        true
-    } catch (ex: ClassNotFoundException) {
-        LoggerFactory.getLogger(Slf4jLoggerProvider::class.java).warn("falling back to SLF4J 1.x compatible binding")
-        false
+    private fun supportsSlf4j2x(logger: org.slf4j.Logger): Boolean {
+        if (useSlf4j2x == null) {
+            useSlf4j2x = try {
+                // Considering Logger#atLevel only exists in slf4j2, this will throw in slf4j1 and proceed without throwing in slf4j2.
+                logger.atLevel(Level.DEBUG)
+                true
+            } catch (e: NoSuchMethodError) {
+                LoggerFactory.getLogger(Slf4jLoggerProvider::class.java).warn("falling back to SLF4J 1.x compatible binding")
+                false
+            }
+        }
+        return useSlf4j2x!!
     }
 
     override fun getOrCreateLogger(name: String): Logger {
         val sl4fjLogger = LoggerFactory.getLogger(name)
-        return if (useSlf4j2x) {
+        return if (supportsSlf4j2x(sl4fjLogger)) {
             Slf4j2xLoggerAdapter(sl4fjLogger)
         } else {
             Slf4j1xLoggerAdapter(sl4fjLogger)
