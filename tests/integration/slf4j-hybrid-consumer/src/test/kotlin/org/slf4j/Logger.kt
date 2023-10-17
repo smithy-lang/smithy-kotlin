50ecd9580932446f6d7a5db3608deeccd5fe062a
natanfudge <natan.lifsiz@gmail.com>
2023-10-17 11:08:14 +0300

    add copyright header and .changes

--- tests/integration/slf4j-hybrid-consumer/src/test/kotlin/org/slf4j/Logger.kt
+++ tests/integration/slf4j-hybrid-consumer/src/test/kotlin/org/slf4j/Logger.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.slf4j
 
 // Override Logger interface to emulate having a slf4j1x interface be classloaded
