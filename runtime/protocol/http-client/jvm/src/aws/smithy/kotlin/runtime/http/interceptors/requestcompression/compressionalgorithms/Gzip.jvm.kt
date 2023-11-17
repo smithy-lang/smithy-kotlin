0d18266b44c31772fb1538ef932fe9b82c514c6e
0marperez <operez899999@gmail.com>
2023-11-17 10:14:35 -0500

    Draft of compression

--- runtime/protocol/http-client/jvm/src/aws/smithy/kotlin/runtime/http/interceptors/requestcompression/compressionalgorithms/Gzip.jvm.kt
+++ runtime/protocol/http-client/jvm/src/aws/smithy/kotlin/runtime/http/interceptors/requestcompression/compressionalgorithms/Gzip.jvm.kt
@@ -6,6 +6,12 @@
 package aws.smithy.kotlin.runtime.http.interceptors.requestcompression.compressionalgorithms
 
 import aws.smithy.kotlin.runtime.http.HttpBody
+import aws.smithy.kotlin.runtime.http.toByteStream
+import aws.smithy.kotlin.runtime.io.SdkBuffer
+import aws.smithy.kotlin.runtime.io.SdkByteReadChannel
+import aws.smithy.kotlin.runtime.io.SdkSource
+import aws.smithy.kotlin.runtime.io.internal.toOkio
+import java.util.zip.GZIPOutputStream
 
 internal actual class Gzip : CompressionAlgorithm {
     actual override val id: String
@@ -14,17 +20,71 @@ internal actual class Gzip : CompressionAlgorithm {
     actual override suspend fun compress(stream: HttpBody): HttpBody {
         when(stream) {
             is HttpBody.SourceContent -> {
-                stream.readFrom()
-                stream.
+                GzipSdkSource(stream.readFrom())
             }
             is HttpBody.ChannelContent -> {
 
             }
             is HttpBody.Bytes -> {
-
+                stream.toByteStream()
             }
             is HttpBody.Empty -> stream
         }
-        TODO("Finish implementation")
+        TODO("Finish implementation ...")
     }
 }
+
+
+HttpBody.SourceContent -> object: HttpBody {
+    override fun readFrom(): SdkSource = GzipSdkSource(stream.readFrom())
+}
+
+private class GzipByteReadChannel(
+    private val delegate: SdkByteReadChannel,
+): SdkByteReadChannel {
+
+    val buffer = SdkBuffer()
+    val gzipOS = GZIPOutputStream(buffer.outputStream())
+
+    override val availableForRead: Int
+        get() = TODO("Not yet implemented")
+    override val isClosedForRead: Boolean
+        get() = TODO("Not yet implemented")
+    override val isClosedForWrite: Boolean
+        get() = TODO("Not yet implemented")
+    override val closedCause: Throwable?
+        get() = TODO("Not yet implemented")
+
+    override suspend fun read(sink: SdkBuffer, limit: Long): Long {
+        val tempBuff = SdkBuffer()
+        delegate.read(tempBuff, 2)
+        val bA = tempBuff.readByteArray()
+        gzipOS.write(bA)
+        buffer.read(sink, 2)
+    }
+
+    override fun cancel(cause: Throwable?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
+
+private class GzipSdkSource( // runtime core / io | look at tests too
+    private val delegate: SdkSource,
+): SdkSource {
+
+    val buffer = SdkBuffer()
+    val gzipOS = GZIPOutputStream(buffer.outputStream())
+
+    override fun read(sink: SdkBuffer, limit: Long): Long {
+        val tempBuff = SdkBuffer()
+        delegate.read(tempBuff, 2)
+        val bA = tempBuff.readByteArray()
+        gzipOS.write(bA)
+        buffer.read(sink, 2)
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+}
+
