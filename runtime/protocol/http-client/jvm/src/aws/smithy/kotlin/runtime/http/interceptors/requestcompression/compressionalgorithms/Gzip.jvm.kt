dcc89f8d55ebd1b76c247d4a6a4a35cd81c7de06
0marperez <operez899999@gmail.com>
2023-11-17 20:28:30 -0500

    Worked on jvm gzip compression

--- runtime/protocol/http-client/jvm/src/aws/smithy/kotlin/runtime/http/interceptors/requestcompression/compressionalgorithms/Gzip.jvm.kt
+++ runtime/protocol/http-client/jvm/src/aws/smithy/kotlin/runtime/http/interceptors/requestcompression/compressionalgorithms/Gzip.jvm.kt
@@ -5,86 +5,71 @@
 
 package aws.smithy.kotlin.runtime.http.interceptors.requestcompression.compressionalgorithms
 
+import aws.smithy.kotlin.runtime.ClientException
 import aws.smithy.kotlin.runtime.http.HttpBody
-import aws.smithy.kotlin.runtime.http.toByteStream
+import aws.smithy.kotlin.runtime.http.toHttpBody
 import aws.smithy.kotlin.runtime.io.SdkBuffer
 import aws.smithy.kotlin.runtime.io.SdkByteReadChannel
 import aws.smithy.kotlin.runtime.io.SdkSource
-import aws.smithy.kotlin.runtime.io.internal.toOkio
+import java.io.ByteArrayOutputStream
 import java.util.zip.GZIPOutputStream
 
-internal actual class Gzip : CompressionAlgorithm {
+public actual class Gzip actual constructor(): CompressionAlgorithm {
     actual override val id: String
         get() = "gzip"
 
-    actual override suspend fun compress(stream: HttpBody): HttpBody {
-        when(stream) {
-            is HttpBody.SourceContent -> {
-                GzipSdkSource(stream.readFrom())
-            }
-            is HttpBody.ChannelContent -> {
-
-            }
-            is HttpBody.Bytes -> {
-                stream.toByteStream()
-            }
-            is HttpBody.Empty -> stream
-        }
-        TODO("Finish implementation ...")
+    actual override suspend fun compress(stream: HttpBody): HttpBody = when(stream) {
+        is HttpBody.SourceContent -> GzipSdkSource(stream.readFrom()).toHttpBody()
+        is HttpBody.ChannelContent -> GzipByteReadChannel(stream.readFrom()).toHttpBody()
+        is HttpBody.Bytes -> compressByteArray(stream.bytes())
+        is HttpBody.Empty -> stream
+        else -> throw ClientException("HttpBody type is not supported")
     }
 }
 
+internal fun compressByteArray(bytes: ByteArray): HttpBody {
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    val gzipOutputStream = GZIPOutputStream(byteArrayOutputStream)
 
-HttpBody.SourceContent -> object: HttpBody {
-    override fun readFrom(): SdkSource = GzipSdkSource(stream.readFrom())
+    gzipOutputStream.write(bytes)
+    gzipOutputStream.close()
+
+    val compressedBody = byteArrayOutputStream.toByteArray().toHttpBody()
+    byteArrayOutputStream.close()
+
+    return compressedBody
 }
 
-private class GzipByteReadChannel(
-    private val delegate: SdkByteReadChannel,
-): SdkByteReadChannel {
-
-    val buffer = SdkBuffer()
-    val gzipOS = GZIPOutputStream(buffer.outputStream())
-
-    override val availableForRead: Int
-        get() = TODO("Not yet implemented")
-    override val isClosedForRead: Boolean
-        get() = TODO("Not yet implemented")
-    override val isClosedForWrite: Boolean
-        get() = TODO("Not yet implemented")
-    override val closedCause: Throwable?
-        get() = TODO("Not yet implemented")
-
-    override suspend fun read(sink: SdkBuffer, limit: Long): Long {
-        val tempBuff = SdkBuffer()
-        delegate.read(tempBuff, 2)
-        val bA = tempBuff.readByteArray()
-        gzipOS.write(bA)
-        buffer.read(sink, 2)
-    }
-
-    override fun cancel(cause: Throwable?): Boolean {
-        TODO("Not yet implemented")
-    }
-}
-
-private class GzipSdkSource( // runtime core / io | look at tests too
+internal class GzipSdkSource(
     private val delegate: SdkSource,
-): SdkSource {
+): SdkSource by delegate {
+    private val buffer = SdkBuffer()
+    private val gzipOS = GZIPOutputStream(buffer.outputStream())
 
-    val buffer = SdkBuffer()
-    val gzipOS = GZIPOutputStream(buffer.outputStream())
-
+    // TODO: Deal with edge cases and maybe change limits
     override fun read(sink: SdkBuffer, limit: Long): Long {
         val tempBuff = SdkBuffer()
-        delegate.read(tempBuff, 2)
+        delegate.read(tempBuff, 1)
         val bA = tempBuff.readByteArray()
         gzipOS.write(bA)
-        buffer.read(sink, 2)
-    }
-
-    override fun close() {
-        TODO("Not yet implemented")
+        buffer.read(sink, 1)
+        return -1
     }
 }
 
+internal class GzipByteReadChannel(
+    private val delegate: SdkByteReadChannel,
+): SdkByteReadChannel by delegate {
+    private val buffer = SdkBuffer()
+    private val gzipOS = GZIPOutputStream(buffer.outputStream())
+
+    // TODO: Deal with edge cases and maybe change limits
+    override suspend fun read(sink: SdkBuffer, limit: Long): Long {
+        val tempBuff = SdkBuffer()
+        delegate.read(tempBuff, 1)
+        val bA = tempBuff.readByteArray()
+        gzipOS.write(bA)
+        buffer.read(sink, 1)
+        return -1
+    }
+}
