3547517142306f9c79754c287b7d0877d05f4edf
Matas Lauzadis <lauzmata@amazon.com>
2023-12-05 15:27:36 -0500

    Add convenience function

--- runtime/auth/aws-credentials/common/src/aws/smithy/kotlin/runtime/auth/awscredentials/CachedCredentialsProvider.kt
+++ runtime/auth/aws-credentials/common/src/aws/smithy/kotlin/runtime/auth/awscredentials/CachedCredentialsProvider.kt
@@ -74,3 +74,21 @@ public class CachedCredentialsProvider(
         source.closeIfCloseable()
     }
 }
+
+/**
+ * A utility function which wraps a [CredentialsProvider] in a [CachedCredentialsProvider].
+ *
+ * @param expireCredentialsAfter the default expiration time period for sourced credentials. For a given set of
+ * cached credentials, the refresh time period will be the minimum of this time and any expiration timestamp on
+ * the credentials themselves.
+ * @param refreshBufferWindow amount of time before the actual credential expiration time when credentials are
+ * considered expired. For example, if credentials are expiring in 15 minutes, and the buffer time is 10 seconds,
+ * then any requests made after 14 minutes and 50 seconds will load new credentials. Defaults to 10 seconds.
+ * @param clock the source of time for this provider
+ * @return the newly-constructed credentials provider
+ */
+public fun CredentialsProvider.cached(
+    expireCredentialsAfter: Duration = DEFAULT_CREDENTIALS_REFRESH_SECONDS.seconds,
+    refreshBufferWindow: Duration = DEFAULT_CREDENTIALS_REFRESH_BUFFER_SECONDS.seconds,
+    clock: Clock = Clock.System,
+): CachedCredentialsProvider = CachedCredentialsProvider(this, expireCredentialsAfter, refreshBufferWindow, clock)
