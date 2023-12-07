82e99bfba8e8c78fd00c193545e40245f48b8567
Aaron J Todd <todaaron@amazon.com>
2023-12-07 15:19:31 -0500

    refactor exception base class generation to not go through protocol generator

--- codegen/smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/rendering/ExceptionBaseClassGenerator.kt
+++ codegen/smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/rendering/ExceptionBaseClassGenerator.kt
@@ -10,42 +10,34 @@ import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.kotlin.codegen.KotlinSettings
 import software.amazon.smithy.kotlin.codegen.core.*
 import software.amazon.smithy.kotlin.codegen.integration.SectionId
+import software.amazon.smithy.kotlin.codegen.integration.SectionKey
 import software.amazon.smithy.kotlin.codegen.model.buildSymbol
-import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
+import software.amazon.smithy.kotlin.codegen.model.namespace
 import software.amazon.smithy.model.knowledge.TopDownIndex
 
 /**
  * Renders the base class that all (modeled) exceptions inherit from.
  * Protocol generators are allowed to override this but they MUST inherit from the base `ServiceException`
  * with the expected constructors.
+ *
  */
 object ExceptionBaseClassGenerator {
+
+    val DefaultServiceExceptionSymbol: Symbol = buildSymbol {
+        name = "ServiceException"
+        namespace(KotlinDependency.CORE)
+    }
 
     /**
      * Defines a section in which code can be added to the body of the base exception type.
      */
-    object ExceptionBaseClassSection : SectionId
+    object ExceptionBaseClassSection : SectionId {
+        val CodegenContext: SectionKey<CodegenContext> = SectionKey("CodegenContext")
+    }
 
     fun render(ctx: CodegenContext, writer: KotlinWriter) {
-        val baseException = ctx.protocolGenerator?.exceptionBaseClassSymbol ?: ProtocolGenerator.DefaultServiceExceptionSymbol
-        writer.addImport(baseException)
-        val serviceException = baseExceptionSymbol(ctx.settings).also { checkForCollision(ctx, it) }
-
-        val name = clientName(ctx.settings.sdkId)
-        writer.dokka("Base class for all service related exceptions thrown by the $name client")
-        writer.withBlock(
-            "#L open class #T : #T {",
-            "}",
-            ctx.settings.api.visibility,
-            serviceException,
-            baseException,
-        ) {
-            write("public constructor() : super()")
-            write("public constructor(message: String?) : super(message)")
-            write("public constructor(message: String?, cause: Throwable?) : super(message, cause)")
-            write("public constructor(cause: Throwable?) : super(cause)")
-
-            writer.declareSection(ExceptionBaseClassSection)
+        writer.declareSection(ExceptionBaseClassSection, mapOf(ExceptionBaseClassSection.CodegenContext to ctx)) {
+            ServiceExceptionBaseClassGenerator().render(ctx, writer)
         }
     }
 
@@ -58,6 +50,39 @@ object ExceptionBaseClassGenerator {
         namespace = "${settings.pkg.name}.model"
         definitionFile = "$name.kt"
     }
+}
+
+/**
+ * Re-usable component for generating the base exception that all generated error shapes will inherit from.
+ * @param parent symbol to inherit from (defaults to [ExceptionBaseClassGenerator.DefaultServiceExceptionSymbol])
+ */
+open class ServiceExceptionBaseClassGenerator(
+    private val parent: Symbol = ExceptionBaseClassGenerator.DefaultServiceExceptionSymbol,
+) {
+    fun render(ctx: CodegenContext, writer: KotlinWriter) {
+        val serviceException = ExceptionBaseClassGenerator.baseExceptionSymbol(ctx.settings).also { checkForCollision(ctx, it) }
+
+        val name = clientName(ctx.settings.sdkId)
+        writer.dokka("Base class for all service related exceptions thrown by the $name client")
+        writer.withBlock(
+            "#L open class #T : #T {",
+            "}",
+            ctx.settings.api.visibility,
+            serviceException,
+            parent,
+        ) {
+            write("public constructor() : super()")
+            write("public constructor(message: String?) : super(message)")
+            write("public constructor(message: String?, cause: Throwable?) : super(message, cause)")
+            write("public constructor(cause: Throwable?) : super(cause)")
+            renderExtra(ctx, writer)
+        }
+    }
+
+    /**
+     * Hook for subclasses to render additional overrides or methods on the base exception
+     */
+    protected open fun renderExtra(ctx: CodegenContext, writer: KotlinWriter) { }
 
     // Compare generated base exception name with all error type names.  Throw exception if not unique.
     private fun checkForCollision(ctx: CodegenContext, exceptionSymbol: Symbol) {
