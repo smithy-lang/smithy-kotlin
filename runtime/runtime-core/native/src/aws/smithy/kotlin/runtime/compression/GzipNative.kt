fcc54c292596d83c3735c1c0560a6a9d8df75f8a
0marperez <operez899999@gmail.com>
2023-12-12 15:32:25 -0500

    Fix native implementations

--- runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/compression/GzipNative.kt
+++ runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/compression/GzipNative.kt
@@ -2,7 +2,7 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-package aws.smithy.kotlin.runtime.http.compression
+package aws.smithy.kotlin.runtime.compression
 
 import aws.smithy.kotlin.runtime.InternalApi
 
--- runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/io/GzipByteReadChannelNative.kt
+++ runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/io/GzipByteReadChannelNative.kt
@@ -2,6 +2,7 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+package aws.smithy.kotlin.runtime.io
 
 import aws.smithy.kotlin.runtime.InternalApi
 
