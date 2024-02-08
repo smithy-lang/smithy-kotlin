615f9f4a5069cd709931dc232d89c5e8b75da1b8
Matas Lauzadis <lauzmata@amazon.com>
2024-02-08 10:30:11 -0500

    Add a test for locking the cache

--- runtime/runtime-core/common/test/aws/smithy/kotlin/runtime/collections/LruCacheTest.kt
+++ runtime/runtime-core/common/test/aws/smithy/kotlin/runtime/collections/LruCacheTest.kt
@@ -4,9 +4,14 @@
  */
 package aws.smithy.kotlin.runtime.collections
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withTimeout
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
 
 public class LruCacheTest {
     @Test
@@ -40,5 +45,26 @@ public class LruCacheTest {
         cache.put("c", "3")
         assertEquals(2, cache.size)
         assertEquals("b, c", cache.entries.joinToString { it.key })
+    }
+
+    @Test
+    fun testMutexLocks() = runTest {
+        val cache = LruCache<String, String>(2)
+
+        launch {
+           cache.withLock {
+               delay(5000L)
+           }
+        }
+
+        launch {
+            assertFails {
+                withTimeout(1000L) {
+                    cache.withLock {
+                        println("Mom said it's my turn to use the cache!")
+                    }
+                }
+            }
+        }
     }
 }
