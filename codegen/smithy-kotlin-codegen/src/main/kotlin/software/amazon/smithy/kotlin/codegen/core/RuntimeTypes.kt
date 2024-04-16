547185179cfc52f6de265d971cf97bf4603240c7
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2024-04-16 15:03:28 +0000

    fix: apply clock skew interceptor to clients created via `invoke`

--- codegen/smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/core/RuntimeTypes.kt
+++ codegen/smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/core/RuntimeTypes.kt
@@ -189,6 +189,7 @@ object RuntimeTypes {
     object SmithyClient : RuntimeTypePackage(KotlinDependency.SMITHY_CLIENT) {
         val SdkClient = symbol("SdkClient")
         val AbstractSdkClientBuilder = symbol("AbstractSdkClientBuilder")
+        val AbstractSdkClientFactory = symbol("AbstractSdkClientFactory")
         val LogMode = symbol("LogMode")
         val RetryClientConfig = symbol("RetryClientConfig")
         val RetryStrategyClientConfig = symbol("RetryStrategyClientConfig")
