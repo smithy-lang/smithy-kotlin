c761b629e4ca8767ad1fa3f2f4d2931a085b38fc
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2024-05-03 15:23:58 +0000

    address PR feedback: clean up version name and add KDocs/comments

--- runtime/protocol/http-client-engines/test-suite/jvm/test/aws/smithy/kotlin/runtime/http/test/MitmContainer.kt
+++ runtime/protocol/http-client-engines/test-suite/jvm/test/aws/smithy/kotlin/runtime/http/test/MitmContainer.kt
@@ -19,12 +19,17 @@ private val PROXY_SCRIPT_ROOT = System.getProperty("MITM_PROXY_SCRIPTS_ROOT") //
 // Port used for communication with container
 private val exposedPort = ExposedPort.tcp(CONTAINER_PORT)
 
+/**
+ * A Docker container which runs the **mitmproxy** image. Upon instantiating this class, a docker container will be
+ * created and ran with a logger attached echoing logs out to **STDOUT**. The container will be stopped and removed when
+ * [close] is called.
+ */
 class MitmContainer(vararg options: String) : Closeable {
     private val delegate: Docker.Container
 
     init {
         val cmd = listOf(
-            "mitmdump",
+            "mitmdump", // https://docs.mitmproxy.org/stable/#mitmdump
             "--flow-detail",
             "2",
             "-s",
@@ -32,7 +37,7 @@ class MitmContainer(vararg options: String) : Closeable {
             *options,
         ).also { println("Initializing container with command: $it") }
 
-        // Make proxy scripts from host available to container
+        // Make proxy scripts from host filesystem available in container's filesystem
         val binding = Bind(PROXY_SCRIPT_ROOT, Volume(CONTAINER_MOUNT_POINT), AccessMode.ro)
 
         delegate = Docker.Instance.createContainer(IMAGE_NAME, cmd, binding, exposedPort)
@@ -48,6 +53,9 @@ class MitmContainer(vararg options: String) : Closeable {
         }
     }
 
+    /**
+     * Gets the host port that can be used to communicate to the MITM proxy
+     */
     val hostPort: Int
         get() = delegate.hostPort
 
