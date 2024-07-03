6db7b63ef4744e8c689e4f7832962161a2f3ceb2
Matas Lauzadis <lauzmata@amazon.com>
2024-07-03 13:48:49 -0400

    relocate decodeArgument

--- runtime/serde/serde-cbor/common/src/aws/smithy/kotlin/runtime/serde/cbor/Minor.kt
+++ runtime/serde/serde-cbor/common/src/aws/smithy/kotlin/runtime/serde/cbor/Minor.kt
@@ -4,7 +4,11 @@
  */
 package aws.smithy.kotlin.runtime.serde.cbor
 
+import aws.smithy.kotlin.runtime.io.SdkBuffer
 import aws.smithy.kotlin.runtime.io.SdkBufferedSource
+import aws.smithy.kotlin.runtime.io.readFully
+import aws.smithy.kotlin.runtime.io.use
+import aws.smithy.kotlin.runtime.serde.DeserializationException
 
 /**
  * Represents CBOR minor types (aka "additional information")
@@ -31,4 +35,26 @@ internal val MINOR_BYTE_MASK: UByte = 0b11111u
 internal fun peekMinorByte(buffer: SdkBufferedSource): UByte {
     val byte = buffer.peek().readByte().toUByte()
     return byte and MINOR_BYTE_MASK
+}
+
+internal fun decodeArgument(buffer: SdkBufferedSource): ULong {
+    val minor = buffer.readByte().toUByte() and MINOR_BYTE_MASK
+
+    if (minor < Minor.ARG_1.value) {
+        return minor.toULong()
+    }
+
+    val numBytes = when (minor) {
+        Minor.ARG_1.value -> 1L
+        Minor.ARG_2.value -> 2L
+        Minor.ARG_4.value -> 4L
+        Minor.ARG_8.value -> 8L
+        else -> throw DeserializationException("Unsupported minor value $minor, expected one of ${Minor.ARG_1.value}, ${Minor.ARG_2.value}, ${Minor.ARG_4.value}, ${Minor.ARG_8.value}")
+    }
+
+    val bytes = SdkBuffer().use {
+        buffer.readFully(it, numBytes)
+        it.readByteArray()
+    }
+    return bytes.toULong()
 }
