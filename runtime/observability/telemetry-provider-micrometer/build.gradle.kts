168e3016e123d600a0f7489544651aa0af6a7f13
monosoul <Kloz.Klaud@gmail.com>
2024-07-24 16:08:19 +0200

    MicroMeter -> Micrometer

--- runtime/observability/telemetry-provider-micrometer/build.gradle.kts
+++ runtime/observability/telemetry-provider-micrometer/build.gradle.kts
@@ -2,8 +2,8 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-description = "Telemetry provider based on MicroMeter"
-extra["displayName"] = "Smithy :: Kotlin :: Observability :: MicroMeter Provider"
+description = "Telemetry provider based on Micrometer"
+extra["displayName"] = "Smithy :: Kotlin :: Observability :: Micrometer Provider"
 extra["moduleName"] = "aws.smithy.kotlin.runtime.telemetry.micrometer"
 
 kotlin {
