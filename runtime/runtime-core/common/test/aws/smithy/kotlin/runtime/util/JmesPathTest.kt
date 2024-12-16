a4734c7551694dd90a71aa5d1e4818ec9ae7b47f
0marperez <60363173+0marperez@users.noreply.github.com>
2024-12-16 11:30:46 -0500

    fix: create JMESPATH flattenIfPossible functions for Lists (#1169)

---
+++ runtime/runtime-core/common/test/aws/smithy/kotlin/runtime/util/JmesPathTest.kt
@@ -0,0 +1,49 @@
+package aws.smithy.kotlin.runtime.util
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class JmesPathTest {
+    @Test
+    fun flattenNestedLists() {
+        val nestedList = listOf(
+            listOf(1, 2, 3),
+            listOf(4, 5),
+            listOf(6),
+        )
+        val flattenedList = nestedList.flattenIfPossible()
+        assertEquals(listOf(1, 2, 3, 4, 5, 6), flattenedList)
+    }
+
+    @Test
+    fun flattenEmptyNestedLists() {
+        val nestedList = listOf(
+            listOf<Int>(),
+            listOf(),
+            listOf(),
+        )
+        val flattenedList = nestedList.flattenIfPossible()
+        assertTrue(flattenedList.isEmpty())
+    }
+
+    @Test
+    fun flattenNestedEmptyAndNonEmptyNestedLists() {
+        val nestedList = listOf(
+            listOf(1, 2),
+            listOf(),
+            listOf(3, 4, 5),
+        )
+        val flattenedList = nestedList.flattenIfPossible()
+        assertEquals(listOf(1, 2, 3, 4, 5), flattenedList)
+    }
+
+    @Test
+    fun flattenList() {
+        val nestedList = listOf(
+            listOf(1, 2, 3),
+        )
+        val flattenedList = nestedList.flattenIfPossible()
+        assertEquals(listOf(1, 2, 3), flattenedList)
+    }
+}
