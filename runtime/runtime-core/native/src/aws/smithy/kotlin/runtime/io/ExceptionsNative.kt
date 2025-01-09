fd21fa09407d852dfb753d16d32050441b3dce88
Matas Lauzadis <lauzmata@amazon.com>
2025-01-09 17:26:31 -0500

    Pass `cause` to super class

--- runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/io/ExceptionsNative.kt
+++ runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/io/ExceptionsNative.kt
@@ -12,7 +12,7 @@ public actual open class IOException actual constructor(
     public actual constructor(message: String?) : this(message, null)
 }
 
-public actual open class EOFException actual constructor(message: String?, cause: Throwable?) : IOException(message) {
+public actual open class EOFException actual constructor(message: String?, cause: Throwable?) : IOException(message, cause) {
     public actual constructor() : this(null, null)
     public actual constructor(message: String?) : this(message, null)
 }
