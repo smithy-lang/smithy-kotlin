c325a4c9825ef94132b7d8d4d8dd15dce471bacf
Matas Lauzadis <lauzmata@amazon.com>
2025-02-03 20:53:33 -0500

    Upgrade to latest build plugin

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -116,54 +116,6 @@ subprojects {
             }
         }
     }
-
-    smithyConfigureIosSimulatorTasks()
 }
 
-/**
- * Disables standalone mode in simulator tests since it causes issues with TLS.
- * This means we need to manage the simulator state ourselves (booting, shutting down).
- * https://youtrack.jetbrains.com/issue/KT-38317
- */
-public fun Project.smithyConfigureIosSimulatorTasks() {
-    if (!HostManager.hostIsMac) return
-
-    val simulatorDeviceName = project.findProperty("iosSimulatorDevice") as? String ?: "iPhone 15"
-    val xcrun = "/usr/bin/xcrun"
-
-    val bootTask = rootProject.tasks.maybeCreate("bootIosSimulatorDevice", Exec::class.java).apply {
-        isIgnoreExitValue = true
-        commandLine(xcrun, "simctl", "boot", simulatorDeviceName)
-
-        doLast {
-            val result = executionResult.get()
-            val code = result.exitValue
-            if (code != 148 && code != 149) { // ignore "simulator already running" errors
-                result.assertNormalExitValue()
-            }
-        }
-    }
-
-    val shutdownTask = rootProject.tasks.maybeCreate("shutdownIosSimulatorDevice", Exec::class.java).apply {
-        isIgnoreExitValue = true
-        commandLine(xcrun, "simctl", "shutdown", simulatorDeviceName)
-
-        doLast {
-            val result = executionResult.get()
-            val code = result.exitValue
-            if (code != 148 && code != 149) { // ignore "simulator already shutdown" errors
-                result.assertNormalExitValue()
-            }
-        }
-    }
-
-    allprojects {
-        val simulatorTasks = tasks.withType<KotlinNativeSimulatorTest>()
-        simulatorTasks.configureEach {
-            dependsOn(bootTask)
-            standalone.set(false)
-            device.set(simulatorDeviceName)
-        }
-        shutdownTask.mustRunAfter(simulatorTasks)
-    }
-}
+configureIosSimulatorTasks()
