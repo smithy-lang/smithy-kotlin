026c4ca3395ac8b6cffd749f5433be0570c05fb2
Matas Lauzadis <lauzmata@amazon.com>
2025-04-09 21:56:12 -0400

    Add TODO implementation on Native

--- runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/util/PlatformNative.kt
+++ runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/util/PlatformNative.kt
@@ -16,6 +16,9 @@ internal actual object SystemDefaultProvider : PlatformProvider {
     actual override val filePathSeparator: String
         get() = TODO("Not yet implemented")
 
+    actual override val lineSeparator: String
+        get() = TODO("Not yet implemented")
+
     actual override suspend fun readFileOrNull(path: String): ByteArray? {
         TODO("Not yet implemented")
     }
