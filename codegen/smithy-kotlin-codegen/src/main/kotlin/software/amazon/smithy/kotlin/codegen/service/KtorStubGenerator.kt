fb01ca36057219f6ce55e85409f80c67d0aba10c
luigi <luigiliu@amazon.com>
2025-07-15 14:01:17 -0700

    fix

--- codegen/smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/service/KtorStubGenerator.kt
+++ codegen/smithy-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/service/KtorStubGenerator.kt
@@ -1,6 +1,5 @@
 package software.amazon.smithy.kotlin.codegen.service
 
-import software.amazon.smithy.aws.traits.auth.SigV4Trait
 import software.amazon.smithy.build.FileManifest
 import software.amazon.smithy.kotlin.codegen.core.GenerationContext
 import software.amazon.smithy.kotlin.codegen.core.InlineCodeWriterFormatter
@@ -263,7 +262,6 @@ internal class KtorStubGenerator(
         val hasServiceHttpBearerAuthTrait = serviceShape.hasTrait(HttpBearerAuthTrait.ID)
         val authTrait = shape.getTrait<AuthTrait>()
         val hasOperationBearerAuthTrait = authTrait?.valueSet?.contains(HttpBearerAuthTrait.ID) ?: true
-        val hasOperationSigV4AuthTrait = authTrait?.valueSet?.contains(SigV4Trait.ID) ?: true
 
         if (hasServiceHttpBearerAuthTrait && hasOperationBearerAuthTrait) {
             w.write(
