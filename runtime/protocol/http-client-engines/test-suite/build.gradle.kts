ff2a2ebe508f7edc30a271c560603fb445916226
Matas Lauzadis <lauzmata@amazon.com>
2025-08-14 18:58:27 -0400

    Revert "Address Gradle configuration cache issues"
    This reverts commit 04c109050034c9e41e69e4699ee2c0c73038d28a.

--- runtime/protocol/http-client-engines/test-suite/build.gradle.kts
+++ runtime/protocol/http-client-engines/test-suite/build.gradle.kts
@@ -110,9 +110,9 @@ val startTestServers = task<LocalTestServers>("startTestServers") {
 val testTasks = listOf("allTests", "jvmTest")
     .forEach {
         tasks.named(it) {
-            dependsOn("startTestServers")
+            dependsOn(startTestServers)
             doLast {
-                (project.tasks.getByName("startTestServers") as LocalTestServers).stop()
+                startTestServers.stop()
             }
         }
     }
@@ -120,9 +120,7 @@ val testTasks = listOf("allTests", "jvmTest")
 tasks.jvmTest {
     // set test environment for proxy tests
     systemProperty("MITM_PROXY_SCRIPTS_ROOT", projectDir.resolve("proxy-scripts").absolutePath)
-    systemProperty("SSL_CONFIG_PATH", providers.provider { 
-        (tasks.getByName("startTestServers") as LocalTestServers).sslConfigPath 
-    })
+    systemProperty("SSL_CONFIG_PATH", startTestServers.sslConfigPath)
 
     val enableProxyTestsProp = "aws.test.http.enableProxyTests"
     val runningInCodeBuild = System.getenv().containsKey("CODEBUILD_BUILD_ID")
