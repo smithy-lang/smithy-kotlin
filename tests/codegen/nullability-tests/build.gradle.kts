ae0a41c4b47e272d2eb2d45c62ec4c892d8c8178
Matas <lauzmata@amazon.com>
2025-08-27 11:29:02 -0400

    misc: enable `allWarningsAsErrors` (#1395)

--- tests/codegen/nullability-tests/build.gradle.kts
+++ tests/codegen/nullability-tests/build.gradle.kts
@@ -45,8 +45,6 @@ kotlin.sourceSets.getByName("main") {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     dependsOn(tasks.generateSmithyProjections)
-    // FIXME - generated code has warnings unfortunately, see https://github.com/awslabs/aws-sdk-kotlin/issues/1169
-    compilerOptions.allWarningsAsErrors = false
 }
 
 tasks.test {
