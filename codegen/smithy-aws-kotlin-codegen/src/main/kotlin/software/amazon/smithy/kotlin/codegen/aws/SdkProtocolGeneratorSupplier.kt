174fee822adafcffc0b8499a41c2af7f8692089b
0marperez <operez899999@gmail.com>
2025-12-30 10:42:48 -0500

    register generic protocols

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/aws/SdkProtocolGeneratorSupplier.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/software/amazon/smithy/kotlin/codegen/aws/SdkProtocolGeneratorSupplier.kt
@@ -6,7 +6,6 @@ package software.amazon.smithy.kotlin.codegen.aws
 
 import software.amazon.smithy.kotlin.codegen.aws.protocols.*
 import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
-import software.amazon.smithy.kotlin.codegen.protocols.RpcV2Cbor
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
 
 /**
@@ -29,6 +28,5 @@ class SdkProtocolGeneratorSupplier : KotlinIntegration {
             RestXml(),
             AwsQuery(),
             Ec2Query(),
-            RpcV2Cbor(),
         )
 }
