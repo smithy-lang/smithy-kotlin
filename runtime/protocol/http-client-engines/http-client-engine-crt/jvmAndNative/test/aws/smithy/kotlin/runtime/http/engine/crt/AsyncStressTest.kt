14d9fe71b903465b610fecd6439fd55fe95b17ce
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2025-12-30 17:16:59 +0000

    Remove wait=true, add more debug output

--- runtime/protocol/http-client-engines/http-client-engine-crt/jvmAndNative/test/aws/smithy/kotlin/runtime/http/engine/crt/AsyncStressTest.kt
+++ runtime/protocol/http-client-engines/http-client-engine-crt/jvmAndNative/test/aws/smithy/kotlin/runtime/http/engine/crt/AsyncStressTest.kt
@@ -8,6 +8,7 @@ package aws.smithy.kotlin.runtime.http.engine.crt
 import aws.smithy.kotlin.runtime.http.HttpMethod
 import aws.smithy.kotlin.runtime.http.SdkHttpClient
 import aws.smithy.kotlin.runtime.http.complete
+import aws.smithy.kotlin.runtime.http.readAll
 import aws.smithy.kotlin.runtime.http.request.HttpRequestBuilder
 import aws.smithy.kotlin.runtime.http.request.url
 import aws.smithy.kotlin.runtime.httptest.TestWithLocalServer
@@ -18,10 +19,8 @@ import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
-import kotlinx.coroutines.yield
+import kotlinx.atomicfu.atomic
+import kotlinx.coroutines.*
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.seconds
 
@@ -44,7 +43,7 @@ class AsyncStressTest : TestWithLocalServer() {
                 call.respondText(text.repeat(respSize / text.length))
             }
         }
-    }.start(wait = true)
+    }.start()
 
     @Test
     fun testStreamNotConsumed() = runBlocking {
@@ -52,33 +51,64 @@ class AsyncStressTest : TestWithLocalServer() {
         // appropriately and allows requests to proceed (a stream that isn't consumed will be in a stuck state
         // if the window is full and never incremented again, this can lead to all connections being consumed
         // and the engine to no longer make further requests)
-        CrtHttpEngine().use { engine ->
-            val client = SdkHttpClient(engine)
-            val request = HttpRequestBuilder().apply {
-                url {
-                    scheme = Scheme.HTTP
-                    method = HttpMethod.GET
-                    host = Host.Domain(testHost)
-                    port = serverPort
-                    path.decoded = "/largeResponse"
-                }
-            }
 
-            withTimeout(30.seconds) {
-                repeat(1_000) {
-                    async {
-                        try {
-                            val call = client.call(request)
-                            yield()
-                            call.complete()
-                        } catch (ex: Exception) {
-                            println("exception on $it: $ex")
-                            throw ex
-                        }
+        val tasksStarted = atomic(0)
+        val callsStarted = atomic(0)
+        val yieldsCompleted = atomic(0)
+        val callsCompleted = atomic(0)
+        val cancelExceptions = atomic(0)
+        val nonCancelExceptions = atomic(0)
+
+        try {
+            CrtHttpEngine().use { engine ->
+                val client = SdkHttpClient(engine)
+                val request = HttpRequestBuilder().apply {
+                    url {
+                        scheme = Scheme.HTTP
+                        method = HttpMethod.GET
+                        host = Host.Domain(testHost)
+                        port = serverPort
+                        path.decoded = "/largeResponse"
                     }
-                    yield()
+                }
+
+                // Sanity check: verify that a single request goes through
+                val sanityCall = client.call(request)
+                val resp = sanityCall.response.body.readAll()
+                sanityCall.complete()
+                println("Successfully completed sanity check call (response length = ${resp?.size}")
+
+                withTimeout(30.seconds) {
+                    repeat(1_000) {
+                        async {
+                            tasksStarted.incrementAndGet()
+                            try {
+                                val call = client.call(request)
+                                callsStarted.incrementAndGet()
+                                yield()
+                                yieldsCompleted.incrementAndGet()
+                                call.complete()
+                                callsCompleted.incrementAndGet()
+                            } catch (_: CancellationException) {
+                                // expected
+                                cancelExceptions.incrementAndGet()
+                            } catch (ex: Exception) {
+                                nonCancelExceptions.incrementAndGet()
+                                println("exception on $it: $ex")
+                                throw ex
+                            }
+                        }
+                        yield()
+                    }
                 }
             }
+        } finally {
+            println("Tasks started: $tasksStarted")
+            println("Calls started: $callsStarted")
+            println("Yields completed: $yieldsCompleted")
+            println("Calls completed: $callsCompleted")
+            println("CancellationExceptions: $cancelExceptions")
+            println("Other exceptions: $nonCancelExceptions")
         }
     }
 }
