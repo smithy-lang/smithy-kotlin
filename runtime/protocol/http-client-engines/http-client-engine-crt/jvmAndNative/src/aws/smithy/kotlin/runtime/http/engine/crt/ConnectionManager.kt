3efbe7b378cd9ecf9c8a55668c6f65a9d4b015b8
Matas Lauzadis <lauzmata@amazon.com>
2026-02-12 11:07:09 -0500

    reduce timeout significantly

--- runtime/protocol/http-client-engines/http-client-engine-crt/jvmAndNative/src/aws/smithy/kotlin/runtime/http/engine/crt/ConnectionManager.kt
+++ runtime/protocol/http-client-engines/http-client-engine-crt/jvmAndNative/src/aws/smithy/kotlin/runtime/http/engine/crt/ConnectionManager.kt
@@ -124,11 +124,11 @@ internal class ConnectionManager(
         connManagers.forEach { entry -> entry.value.close() }
         runBlocking {
             connManagers.forEach { entry ->
-                // FIXME In some cases, we are leaking CRT stream resources somewhere, which leaves a connection open
-                //  Trying to wait for this shutdown hangs indefinitely
+                // FIXME In some test cases, we are leaking CRT stream resources somewhere, which leaves a connection open,
+                //  causing waitForShutdown to hang indefinitely.
                 //  My attempts to properly close the stream introduce segfaults / double-frees.
                 //  Need to find the right place to call crtStream?.close(), or another solution
-                withTimeoutOrNull(5000) {
+                withTimeoutOrNull(50) {
                     entry.value.waitForShutdown()
                 } ?: println("Shutdown timed out for ${entry.key}, continuing...")
             }
