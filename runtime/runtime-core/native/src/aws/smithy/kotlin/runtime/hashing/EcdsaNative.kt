10b9aa3dde81c8c5f7838baccf627714ad1a2f37
0marperez <operez899999@gmail.com>
2026-02-20 16:11:44 -0500

    clean up code

--- runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/hashing/EcdsaNative.kt
+++ runtime/runtime-core/native/src/aws/smithy/kotlin/runtime/hashing/EcdsaNative.kt
@@ -4,20 +4,18 @@
  */
 package aws.smithy.kotlin.runtime.hashing
 
-import aws.sdk.kotlin.crt.util.hashing.destroyKeyPair
-import aws.sdk.kotlin.crt.util.hashing.keyPairFromPrivateKey
+import aws.sdk.kotlin.crt.CrtRuntimeException
+import aws.sdk.kotlin.crt.util.hashing.eccKeyPairFromPrivateKey
+import aws.sdk.kotlin.crt.util.hashing.releaseEccKeyPair
 import aws.sdk.kotlin.crt.util.hashing.signMessage
 import kotlinx.cinterop.ExperimentalForeignApi
 
-/**
- * ECDSA on the SECP256R1 curve.
- */
 @OptIn(ExperimentalForeignApi::class)
 public actual fun ecdsaSecp256r1(key: ByteArray, message: ByteArray): ByteArray {
-    val keyPair = keyPairFromPrivateKey(key) ?: error("Failed to create key pair")
+    val keyPair = eccKeyPairFromPrivateKey(key) ?: throw CrtRuntimeException("Failed to create ECC key pair from private key")
     try {
         return signMessage(keyPair, message)
     } finally {
-        destroyKeyPair(keyPair)
+        releaseEccKeyPair(keyPair)
     }
 }
