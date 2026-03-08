3d93470624a792af34ffe6dc29ab4a1724d1a664
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2026-03-08 22:00:25 +0000

    fix native impls

--- runtime/testing/native/src/aws/smithy/kotlin/runtime/testing/TestAnnotationsNative.kt
+++ runtime/testing/native/src/aws/smithy/kotlin/runtime/testing/TestAnnotationsNative.kt
@@ -11,8 +11,6 @@ public actual typealias AfterAll = kotlin.test.AfterClass
 
 public actual typealias IgnoreNative = kotlin.test.Ignore
 
-public actual typealias TestInstance = kotlin.test.TestInstance
-
 // Effectively ignored on K/N. This is fine because we only use this to enable non-static @BeforeAll/@AfterAll methods
 // on JVM.
 public actual annotation class TestInstance(val value: TestLifecycle)
