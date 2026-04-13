4f420829a245b95ee73e34035b26ccde90d731ae
0marperez <operez899999@gmail.com>
2026-04-13 13:46:43 -0600

    fix: enum init deadlock

---
+++ tests/codegen/enum-tests/build.gradle.kts
@@ -0,0 +1,63 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import aws.sdk.kotlin.gradle.codegen.smithyKotlinProjectionSrcDir
+import aws.sdk.kotlin.gradle.publishing.skipPublishing
+
+plugins {
+    id(libs.plugins.kotlin.jvm.get().pluginId)
+    alias(libs.plugins.aws.kotlin.repo.tools.smithybuild)
+}
+
+skipPublishing()
+
+val codegen by configurations.getting
+dependencies {
+    codegen(project(":codegen:codegen"))
+    codegen(libs.smithy.cli)
+    codegen(libs.smithy.model)
+}
+
+tasks.generateSmithyProjections {
+    smithyBuildConfigs.set(files("smithy-build.json"))
+}
+
+tasks.kotlinSourcesJar {
+    dependsOn(tasks.generateSmithyProjections)
+}
+
+val optinAnnotations = listOf("kotlin.RequiresOptIn", "aws.smithy.kotlin.runtime.InternalApi")
+kotlin.sourceSets.all {
+    optinAnnotations.forEach { languageSettings.optIn(it) }
+}
+
+kotlin.sourceSets.getByName("main") {
+    kotlin.srcDir(smithyBuild.smithyKotlinProjectionSrcDir("enum-tests"))
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    dependsOn(tasks.generateSmithyProjections)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true
+    }
+}
+
+dependencies {
+    compileOnly(project(":codegen:codegen"))
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(project(":runtime:runtime-core"))
+    implementation(project(":runtime:smithy-client"))
+    implementation(project(":runtime:protocol:http-client"))
+    implementation(project(":runtime:observability:telemetry-api"))
+    implementation(project(":runtime:observability:telemetry-defaults"))
+
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.kotlinx.coroutines.test)
+}
