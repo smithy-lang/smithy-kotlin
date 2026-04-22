7baeffd582e0387b243c932b8f5c5f8aba3bf9ce
luigi liu <luigiliu@amazon.com>
2026-04-22 16:31:17 -0400

    remove test that are covered by other tests

--- runtime/runtime-core/jvm/test/aws/smithy/kotlin/runtime/retries/NewRetriesFeatureFlagTest.kt
+++ runtime/runtime-core/jvm/test/aws/smithy/kotlin/runtime/retries/NewRetriesFeatureFlagTest.kt
@@ -6,10 +6,9 @@
 package aws.smithy.kotlin.runtime.retries
 
 import aws.smithy.kotlin.runtime.retries.delay.ExponentialBackoffWithJitter
-import aws.smithy.kotlin.runtime.retries.delay.RetryAwareDelayProvider
 import aws.smithy.kotlin.runtime.retries.delay.StandardExponentialBackoffWithJitter
+import aws.smithy.kotlin.runtime.retries.delay.StandardRetryTokenBucket
 import kotlin.test.*
-import kotlin.time.Duration.Companion.milliseconds
 
 class NewRetriesFeatureFlagTest {
     private val sysPropKey = "smithy.newRetries2026"
@@ -20,63 +19,24 @@ class NewRetriesFeatureFlagTest {
     }
 
     @Test
-    fun testFlagOffUsesOldDefaults() {
+    fun testFlagOffUsesLegacyDelayProvider() {
         System.clearProperty(sysPropKey)
         val strategy = StandardRetryStrategy()
         assertIs<ExponentialBackoffWithJitter>(strategy.config.delayProvider)
-        assertEquals(3, strategy.config.maxAttempts)
     }
 
     @Test
-    fun testFlagOnUsesNewDefaults() {
+    fun testFlagOnUsesStandardDelayProvider() {
         System.setProperty(sysPropKey, "true")
         val strategy = StandardRetryStrategy()
-        assertIs<RetryAwareDelayProvider>(strategy.config.delayProvider)
         assertIs<StandardExponentialBackoffWithJitter>(strategy.config.delayProvider)
     }
 
     @Test
-    fun testFlagOnDynamoDbMaxAttempts4() {
+    fun testFlagOnUsesStandardTokenBucket() {
         System.setProperty(sysPropKey, "true")
-        val strategy = StandardRetryStrategy { serviceName = "dynamodb" }
-        assertEquals(4, strategy.config.maxAttempts)
+        val strategy = StandardRetryStrategy()
+        assertIs<StandardRetryTokenBucket>(strategy.config.tokenBucket)
     }
 
-    @Test
-    fun testFlagOnNonDynamoDbMaxAttempts3() {
-        System.setProperty(sysPropKey, "true")
-        val strategy = StandardRetryStrategy { serviceName = "s3" }
-        assertEquals(3, strategy.config.maxAttempts)
-    }
-
-    @Test
-    fun testFlagOnDynamoDbStreamsMaxAttempts4() {
-        System.setProperty(sysPropKey, "true")
-        val strategy = StandardRetryStrategy { serviceName = "dynamodb streams" }
-        assertEquals(4, strategy.config.maxAttempts)
-    }
-
-    @Test
-    fun testFlagOnDynamoDbStreamsCaseInsensitive() {
-        System.setProperty(sysPropKey, "true")
-        val strategy = StandardRetryStrategy { serviceName = "DynamoDB Streams" }
-        assertEquals(4, strategy.config.maxAttempts)
-    }
-
-    @Test
-    fun testFlagOnDynamoDbExplicitMaxAttemptsHonored() {
-        System.setProperty(sysPropKey, "true")
-        val strategy = StandardRetryStrategy {
-            serviceName = "dynamodb"
-            maxAttempts = 3
-        }
-        assertEquals(3, strategy.config.maxAttempts)
-    }
-
-    @Test
-    fun testFlagOffDynamoDbMaxAttempts3() {
-        System.clearProperty(sysPropKey)
-        val strategy = StandardRetryStrategy { serviceName = "dynamodb" }
-        assertEquals(3, strategy.config.maxAttempts)
-    }
 }
