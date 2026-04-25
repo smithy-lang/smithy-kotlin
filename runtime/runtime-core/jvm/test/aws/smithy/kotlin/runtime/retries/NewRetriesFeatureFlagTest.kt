14d11d5c18bbeba5eb0e399358d9cf6707cb3584
luigi liu <luigiliu@amazon.com>
2026-04-24 20:32:31 -0400

    testing

--- runtime/runtime-core/jvm/test/aws/smithy/kotlin/runtime/retries/NewRetriesFeatureFlagTest.kt
+++ runtime/runtime-core/jvm/test/aws/smithy/kotlin/runtime/retries/NewRetriesFeatureFlagTest.kt
@@ -7,11 +7,19 @@ package aws.smithy.kotlin.runtime.retries
 
 import aws.smithy.kotlin.runtime.retries.delay.ExponentialBackoffWithJitter
 import aws.smithy.kotlin.runtime.retries.delay.StandardRetryTokenBucket
+import aws.smithy.kotlin.runtime.retries.policy.RetryErrorType
+import kotlinx.coroutines.test.runTest
 import kotlin.test.*
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class NewRetriesFeatureFlagTest {
     private val sysPropKey = "smithy.newRetries2026"
+
+    @BeforeTest
+    fun setup() {
+        System.clearProperty(sysPropKey)
+    }
 
     @AfterTest
     fun cleanup() {
@@ -20,11 +28,12 @@ class NewRetriesFeatureFlagTest {
 
     @Test
     fun testFlagOffUsesLegacyDefaults() {
-        System.clearProperty(sysPropKey)
         val strategy = StandardRetryStrategy()
         val delayer = assertIs<ExponentialBackoffWithJitter>(strategy.config.delayProvider)
         assertEquals(10.milliseconds, delayer.config.initialDelay)
         assertEquals(1.5, delayer.config.scaleFactor)
+        assertEquals(1.seconds, delayer.config.throttlingBaseDelay)
+        assertEquals(5.seconds, delayer.config.retryAfterMaxOvershoot)
         val tokenBucket = assertIs<StandardRetryTokenBucket>(strategy.config.tokenBucket)
         assertEquals(5, tokenBucket.config.retryCost)
         assertEquals(10, tokenBucket.config.timeoutRetryCost)
@@ -37,15 +46,27 @@ class NewRetriesFeatureFlagTest {
         val delayer = assertIs<ExponentialBackoffWithJitter>(strategy.config.delayProvider)
         assertEquals(50.milliseconds, delayer.config.initialDelay)
         assertEquals(2.0, delayer.config.scaleFactor)
+        assertEquals(1.seconds, delayer.config.throttlingBaseDelay)
+        assertEquals(5.seconds, delayer.config.retryAfterMaxOvershoot)
         val tokenBucket = assertIs<StandardRetryTokenBucket>(strategy.config.tokenBucket)
         assertEquals(14, tokenBucket.config.retryCost)
         assertEquals(5, tokenBucket.config.timeoutRetryCost)
     }
 
     @Test
-    fun testFlagOnUsesStandardTokenBucket() {
+    fun testFlagOffTransientUsesTimeoutRetryCost() = runTest {
+        val bucket = StandardRetryTokenBucket(StandardRetryTokenBucket.Config {})
+        val token = bucket.acquireToken()
+        token.scheduleRetry(RetryErrorType.Transient)
+        assertEquals(500 - 10, bucket.capacity)
+    }
+
+    @Test
+    fun testFlagOnTransientUsesRetryCost() = runTest {
         System.setProperty(sysPropKey, "true")
-        val strategy = StandardRetryStrategy()
-        assertIs<StandardRetryTokenBucket>(strategy.config.tokenBucket)
+        val bucket = StandardRetryTokenBucket(StandardRetryTokenBucket.Config {})
+        val token = bucket.acquireToken()
+        token.scheduleRetry(RetryErrorType.Transient)
+        assertEquals(500 - 14, bucket.capacity)
     }
 }
