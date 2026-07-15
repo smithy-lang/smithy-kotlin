1b0ab0e440fa6f183f253d9418c751a766cc003f
luigi liu <luigiliu@amazon.com>
2026-07-15 14:11:58 -0400

    remove jackson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,6 @@ buildscript {
         classpath(libs.kotlinx.atomicfu.plugin)
         // Add our custom gradle build logic to buildscript classpath
         classpath(libs.aws.kotlin.repo.tools.build.support)
-        /*
-        Enforce jackson to a version supported both by dokka and jreleaser:
-        https://github.com/Kotlin/dokka/issues/3472#issuecomment-1929712374
-        https://github.com/Kotlin/dokka/issues/3194#issuecomment-1929382630
-         */
-        classpath(enforcedPlatform("com.fasterxml.jackson:jackson-bom:2.22.1"))
     }
 
     configurations.classpath {
